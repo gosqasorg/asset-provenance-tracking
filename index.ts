b0a9f6c923ec73492504169fa3d8502d966597ee
Harry Pierson <harrypierson@hotmail.com>
2023-06-07 13:51:07 -0700

    new-device endpoint

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { v1 as uuidV1, parse } from 'uuid';
+import * as crypto from 'crypto';
 
 dotenv.config();
 
@@ -8,6 +10,12 @@ const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
+});
+
+app.get('/new-device', (req: Request, res: Response) => {
+    const deviceId = uuidV1();
+    const deviceSecret = crypto.randomBytes(32).toString('hex');
+    res.json({ deviceId, deviceSecret });
 });
 
 app.listen(port, () => {
