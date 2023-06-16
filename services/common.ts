1ceec12d0f4a38d1799af6498a219503ad9b3340
Harry Pierson <harrypierson@hotmail.com>
2023-06-16 15:34:22 -0700

    different FNV implementation

--- services/common.ts
+++ services/common.ts
@@ -1,12 +1,29 @@
-import * as fnv from 'fnv-plus';
-
 export function calculateDeviceID(key: string | Uint8Array): bigint {
-    // if key is a string, convert it to a buffer to ensure it's hex encoded. 
+    // if key is a string, convert it to a buffer 
     key = typeof key === 'string' ? Buffer.from(key, 'hex') : key;
-    // the fnv library only accepts strings, so convert the key Uint8Array to a lower cased hex string
-    const $key = Buffer.from(key).toString('hex').toLowerCase();
-    // calculate the 64 bit hash and convert it to a bigint
-    const hash = fnv.hash($key, 64).dec();
-    return BigInt(hash);
+    return fnv1(key);
 }
 
+// simple FNV implementation from https://github.com/namralkeeg/fnvjs/
+// https://github.com/tjwebb/fnv-plus may be faster, but it only works on strings
+
+const fnvPrime = 1099511628211n
+const fnvOffset = 14695981039346656037n
+
+export function fnv1(input: Uint8Array) {
+    let hash =  fnvOffset;
+    for (let i = 0; i < input.length; i++) {
+        hash = BigInt.asUintN(64, hash * fnvPrime)
+        hash ^= BigInt(input[i])
+    }
+    return hash;
+}
+
+// function fnv1a(input: Uint8Array) {
+//     let hash =  fnvOffset;
+//     for (let i = 0; i < input.length; i++) {
+//         hash ^= BigInt(input[i])
+//         hash = BigInt.asUintN(64, hash * fnvPrime)
+//     }
+//     return hash;
+// }
