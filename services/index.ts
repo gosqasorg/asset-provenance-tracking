d45a050b61b149704be511897753c2b8f25ec8f0
Harry Pierson <harrypierson@hotmail.com>
2023-06-19 09:12:49 -0700

    WIP

--- services/index.ts
+++ services/index.ts
@@ -1,3 +1,5 @@
 export { createSequelizeRepository }  from './sequelizeRepo';
-export { calculateDeviceID } from './common';
+export { calculateDeviceID, createProvenanceRecord } from './common';
 export { Repository, Device, ProvenanceRecord } from './types';
+
+export { createDeviceRepository } from './memoryRepo';
--- services/types.ts
+++ services/types.ts
@@ -4,6 +4,55 @@ export interface Device {
     readonly deviceID: bigint;
 }
 
+export interface DeviceRepository {
+    createDevice(name: string, factory: ProvenanceReportFactory): Promise<Device>;
+    getDevice(key: string | Uint8Array): Promise<Device | null>;
+    getDevices(): Promise<readonly Device[]>;
+}
+
+export interface ProvenanceReport {
+    readonly deviceID: bigint;
+    readonly contents: string;
+    readonly attachments: readonly Pick<ProvenanceAttachment, 'type' | 'reportID'>[];
+    readonly tags: readonly string[];
+    readonly createdAt: Date;
+}
+
+export interface ProvenanceAttachment {
+    readonly deviceID: bigint;
+    readonly reportID: bigint;
+    readonly type: string;
+    readonly data: Uint8Array;
+    readonly createdAt: Date;
+}
+
+export interface CreateReportOptions {
+    readonly attachments?: readonly Pick<ProvenanceAttachment, 'type' | 'data'>[];
+    readonly tags?: readonly string[]
+}
+
+export type ProvenanceReportFactory = (key: string | Uint8Array, contents: string, options?: CreateReportOptions) => Promise<ProvenanceReport>;
+
+export interface ProvenanceRepository {
+    createReport: ProvenanceReportFactory;
+    getReports(key: string | Uint8Array): Promise<readonly ProvenanceReport[]>;
+    getAttachment(key: string | Uint8Array, reportID: bigint): Promise<readonly ProvenanceAttachment[]>;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export interface ProvenanceRecord {
     readonly deviceID: bigint;
     readonly type: string;
@@ -11,6 +60,9 @@ export interface ProvenanceRecord {
     readonly createdAt: Date;
     readonly dataURI: string;
 }
+
+
+
 
 export interface Repository {
     createDevice(name: string): Promise<Device>;
