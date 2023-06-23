5f49cdac59833c752fc8d645c1e40f400be81035
Harry Pierson <harrypierson@hotmail.com>
2023-06-22 18:17:12 -0700

    make all tags lower case and unique

--- public/ProvenanceForm.js
+++ public/ProvenanceForm.js
@@ -1,38 +1,32 @@
 import html from './html.js';
 import TagInput from './TagInput.js'
-import { h } from 'https://unpkg.com/vue@3/dist/vue.runtime.esm-browser.js';
 
 export default {
     name: `ProvenanceForm`,
     data() {
         return {
-            tags: ['php', 'ruby', 'javascript', "   ", 'python', 'java', 'c#', 'scala', 'closure']
+            tags: []
         };
     },
     computed: {
-        nonEmptyTags() { return this.tags.filter(v => !!(v.trim())) }
+        nonEmptyTags() { 
+            const tagSet = new Set(this.tags.map(t => t.trim().toLowerCase()).filter(t => t.length > 0));
+            return [...tagSet];
+        }
     },
     components: { TagInput },
     render() {
-        const input = h(TagInput, {
-            class: "form-control",
-            modelValue: this.tags,
-            onUpdateTags: (value) => { this.tags = value; }
-        });
-        const pre = h('div', this.nonEmptyTags.map(v => h('span', { class: "badge bg-info text-dark mx-1"},  v)));
-        return h('div', null, [ input, pre ])
+        return html`
+<form method="POST" class="col-6">
+    <legend>Create New Provenance Record</legend>
+    <div class="mb-3">
+        <input type="text" class="form-control" name="description" id="provenance-description" required placeholder="Provenance Description" />
+        <label for="provenance-tags" class="form-label mt-3" >Tags (will be converted to lower case and duplicates removed)</label>
+        <${TagInput} modelValue=${this.tags} name="tags" id="provenance-tags" class="form-control" onUpdateTags=${(value) => { this.tags = value }}/>
+        <div class="my-1">${this.nonEmptyTags.map(t => html`<span class="badge bg-info text-dark mx-1">${t}</span>`)}</div>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+`
     }
-
-    // template: `
-// <form method="POST" class="col-6">
-//     <legend>Create New Provenance Records</legend>
-//     <div class="mb-3">
-//         <label for="provenance-description" class="form-label">Description</label>
-//         <input type="text" class="form-control" name="description" id="provenance-description" required />
-//         <label for="provenance-tags" class="form-label" >Tags</label>
-//         <TagInput :modelValue=${this.tags} class="form-control" />
-//         <pre>${JSON.stringify(this.nonEmptyTags)}</pre>
-//     </div>
-//     <button type="submit" class="btn btn-primary">Submit</button>
-// </form>`,
 };
