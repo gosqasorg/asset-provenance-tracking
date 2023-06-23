ddce40e06261a80a64f0f84efe71ebf2aa49a13f
Harry Pierson <harrypierson@hotmail.com>
2023-06-23 08:58:07 -0700

    make app available on local network

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
+import os from 'os';
 import dotenv from 'dotenv';
 
 import { Sequelize } from 'sequelize';
@@ -18,12 +19,21 @@ async function main() {
     const { devices, provenance } = createMemoryRepositories();
     const server = await createFastifyServer(devices, provenance);
 
-    server.listen({ port: port ?? 3000 }, (err, address) => {
+    server.listen({ port: port ?? 3000, host: '0.0.0.0' }, (err, address) => {
         if (err) {
             console.error(err);
             process.exit(1);
         }
-        console.log(`[server]: Server is running at ${address}`);
+
+        const interfaces = Object.entries(os.networkInterfaces())
+            .flatMap(([key, value]) => (value ?? []).map(v => ({ name: key, ...v })))
+            .filter(v => v.family === 'IPv4')
+            .map(v => ({ name: v.name, address: v.address }))
+
+        // console.log(`[server]: Server is running at ${address}`);
+        for (const i of interfaces) {
+            console.log(`\x1b[32m[server]: ${i.name} Server is running at \x1b[33mhttp://${i.address}:${port}\x1b[0m`);
+        }
     });
 }
 
