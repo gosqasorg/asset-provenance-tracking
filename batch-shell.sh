03310cf779df2a65ebd06704846441c09035cc86
Robert L. Read <read.robert@gmail.com>
2023-10-19 13:49:56 -0700

    fixing bug in QR generation

--- batch-shell.sh
+++ batch-shell.sh
@@ -9,4 +9,4 @@ NUMBER_KEYS=$1
 
 # This is a big complicated. Note that the last part fo the pipleline creates a file without
 # putting a ".png" on the name
-curl -H "Content-Type: application/x-www-form-urlencoded" -X GET $BASE_SERVER/manykeys/"MakerFaireBayArea"/$NUMBER_KEYS | jq '.keys .[]' | xargs -L1 -I'{}' qrencode -o {} {}
+curl -H "Content-Type: application/x-www-form-urlencoded" -X GET $BASE_SERVER/manykeys/"MakerFaireBayArea"/$NUMBER_KEYS | jq '.keys .[]' | xargs -L1 -I'{}' qrencode -o {} $BASE_SERVER/provenance/{}
