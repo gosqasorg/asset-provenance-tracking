ef6ee45765376402f08cb61863663d3ea023a222
K42IE <katiepryal@live.com>
2023-11-20 17:04:31 -0800

    Deleted Thumbs up file and refined the CreateDevice Component

--- packages/frontend/components/CreateDevice.vue
+++ packages/frontend/components/CreateDevice.vue
@@ -1,24 +1,27 @@
 <!-- 
     This component is a form used to create a new device that we will track the 
     providence for. 
+
+    Resourses:
+    https://test-utils.vuejs.org/guide/essentials/forms
 -->
 
 <template>
-    <form method="POST" class="col-md-8 card p-3 text-bg-secondary" enctype="multipart/form-data" @submit.prevent="submitForm">
+    <form enctype="multipart/form-data" @submit.prevent="submitForm"> 
         <h3>Create New Device</h3>
         <div id="device-form">
             <input type="text" class="form-control" v-model="name" required placeholder="Device Name">
             <input type="text" class="form-control mt-3" v-model="description" required placeholder="Device Description">
             <div style="display: block;">
-                <label class="form-label mt-2" for="file">Device Image (optional):    </label>
+                <label>Device Image (optional):    </label>
                 <input type="file" class="form-control" accept="image/*" @change="onFileChange" capture="environment" />
             </div>
             <div style="display: block;">
-                <input type="checkbox" id="save-device-key" v-model="saveDeviceKey">
+                <input type="checkbox" v-model="saveDeviceKey">
                 <label for="save-device-key"> Save device key (demo devices only!)</label>
             </div>
         </div>
-        <button id="submit-button" type="submit" class="btn btn-primary">Submit</button>
+        <button id="submit-button" type="submit">Submit</button>
     </form>
 </template>
 
@@ -34,10 +37,16 @@ export default {
     },
     methods: {
         onFileChange(e) {
-            this.picture = e.target.files[0];
+            this.form.picture = e.target.files[0];
         },
-        submitForm() {
+        async submitForm() {
             // Handle form submission here
+            console.log(this.name, this.description, this.saveDeviceKey);
+            // Reset form inputs
+            this.name = '';
+            this.description = '';
+            this.picture = null;
+            this.saveDeviceKey = false;
         }
     }
 }
