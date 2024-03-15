b9cc4a1489c4600c46e90c0855bc3d5fce3dfd20
Robert L. Read <read.robert@gmail.com>
2024-03-15 13:05:10 -0500

    timestamps might be sort of working

--- packages/frontend/components/TimestampList.vue
+++ packages/frontend/components/TimestampList.vue
@@ -3,7 +3,7 @@
     <!-- <h1>Statistics! </h1> -->
     <div>Time Stamp List</div>
      <div>
-    <span v-for="event in pairs">{{ event.gdttimestamp }}, {{event.gtdid}}</span>
+    <div v-for="event in timestamppairs">{{ Date(event.gdttimestamp) }}, {{event.gdtid}}</div>
      </div>
     <div></div>
     <div></div>
@@ -15,23 +15,7 @@ import { getStatistics} from '~/services/azureFuncs';
 import { EventBus } from '~/utils/event-bus';
 
 export default {
-    data() {
-        return {
-            pairs: []
-        }
-    },
-    methods: {
-        refreshPage() {
-            console.log('Statistics refresh event received');
-            getStatistics().then((response) => {
-                console.log(response);
-                console.log(response.body);
-                this.pairs = response;
-            }).catch(error =>  {
-                console.log("Error in TimestampList Getstatitics!");
-                console.log(error);
-            })
-        }
-    }
+    name: 'TimestampList',
+    props: ['timestamppairs']
 };
 </script>
--- packages/frontend/pages/statistics.vue
+++ packages/frontend/pages/statistics.vue
@@ -2,32 +2,81 @@
   <div>
     <!-- <h1>Statistics! </h1> -->
     <div>Well hello there!</div>
-     <div>
-    <TimestampList/>
-     </div>
+    <div v-if="!isLoading">
+    <TimestampList v-bind:timestamppairs="myTimestampPairs"/>
+  </div>
+  <div v-else>
+    Show your prendered data here
+  </div>
     <div></div>
     <div></div>
   </div>
-</template>
+    </template>
+
+    <script>
+    import TimestampList from "@/components/TimestampList.vue";
+
+let tspairs = [
+                {
+                    "gdttimestamp": "1710510353826",
+                    "gdtid": "ffcc",
+                },
+];
+
+export default {
+    components: {
+        TimestampList
+    },
+    data() {
+        return {
+            isLoading: true,
+            myTimestampPairs: tspairs
+        }},
+
+    methods: {
+    async fetchData() {
+        try {
+        const response = await getStatistics();
+        console.log("Maybe theres are the pairs:");
+        console.log(response);
+        tspairs = response;
+            return tspairs;
+        } catch (error) {
+            console.log(error)
+        }
+    },
+  },
+  async mounted(){
+      this.myTimestampPairs = await this.fetchData();
+//      tspairs = this.myTimstampPairs;
+      console.log("a Griffon flew down");
+     this.isLoading = false
+  }
+}
+
+</script>
 
 <script setup>
 import { useRoute } from 'vue-router'
 import { getProvenance,getStatistics} from '~/services/azureFuncs';
 import { EventBus } from '~/utils/event-bus';
-
-const route = useRoute()
-
+import { ref, onMounted } from 'vue'
 console.log("a dragon flew down");
 
-onMounted(async () => {
-    console.log("a Griffon flew down");
-    try {
-        const response = await getStatistics();
-        console.log(response);
-        console.log(response.body);
-    } catch (error) {
-        console.log(error)
-    }
-})
+
+
+
+// onMounted(async () => {
+//     console.log("a Griffon flew down");
+//     try {
+//         const response = await getStatistics();
+//         console.log("Maybe theres are the pairs:");
+//         console.log(response);
+//         tspairs = response;
+//         this.isLoading = false;
+//     } catch (error) {
+//         console.log(error)
+//     }
+// })
 
 </script>
