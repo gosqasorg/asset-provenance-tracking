5317b939649cc72d5275a5a4acdf416320fb6a28
chihlinc <chihlinc@andrew.cmu.edu>
2024-03-30 10:31:11 -0400

    Device key is not found.
    f deviceKey is present, it renders the main content of the page, including the device information, history records, and links to create new history records.
    If deviceKey is not present, it renders a message saying "Device key not found."

--- packages/frontend/pages/provenance/[deviceKey].vue
+++ packages/frontend/pages/provenance/[deviceKey].vue
@@ -4,14 +4,19 @@
 --> 
 <template>
     <div>
-        <h1>"{{ deviceInfo.deviceName }}" Asset History Records</h1>
-        <div>Device ID: {{ deviceKey }}</div>
-        <a href="#createRecord">Go to "Create New History Record"</a>
+        <template v-if="deviceKey">
+            <h1>"{{ deviceInfo.deviceName }}" Asset History Records</h1>
+            <div>Device ID: {{ deviceKey }}</div>
+            <a href="'#createRecord/' + deviceKey">Go to "Create New History Record"</a>
         <div>
             <ProvidenceFeed :deviceKey="deviceKey"/>
         </div>
         <hr class="col-1 my-4">
         <ProvidenceCreateRecord :deviceKey="deviceKey" id="createRecord"/>
+        </template>
+        <template v-else>
+            <p>Device key not found.</p>
+        </template>    
     </div>
 </template>
 
@@ -27,6 +32,10 @@ const deviceKey = route.params.deviceKey
 let deviceInfo = ref({})
 
 onMounted(async () => {
+    if (!deviceKey) {
+        console.error('Device key not provided in URL.')
+        return
+    }
     try {
         const response = await getProvenance(deviceKey)
         deviceInfo.value = response[response.length - 1].record
