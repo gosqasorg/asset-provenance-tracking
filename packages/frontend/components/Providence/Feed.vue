a3a090d9eb36801e5158b12141165b20614e8348
Coco Chen <chihlinc@Cocos-MacBook-Pro-3.local>
2024-05-16 22:36:04 -0400

    Update the download link

--- packages/frontend/components/Providence/Feed.vue
+++ packages/frontend/components/Providence/Feed.vue
@@ -6,34 +6,39 @@
     <div>
         <div v-for="(report, index) in provenance" class="report-box">
             <template v-if="report.record.blobType === 'deviceInitializer'">
-                <h3 id = "createdDevicePoint">Created Device: {{ report.record.deviceName }}</h3>
+                <h3 id="createdDevicePoint">Created Device: {{ report.record.deviceName }}</h3>
                 <div>{{ report.record.deviceDescription }}</div>
             </template>
-
             <template v-else>
-            <div class="mb-1 tag-container">
-    <span class="tag" v-for="tag in report.record.tags" v-bind:style="'color: '+textColorForTag(tag)+'; background-color: '+getColorForTag(tag)+';'">
-    {{tag}}</span>
-            </div>
+                <div class="mb-1 tag-container">
+                    <span class="tag" v-for="tag in report.record.tags" v-bind:style="'color: '+textColorForTag(tag)+'; background-color: '+getColorForTag(tag)+';'">
+                        {{ tag }}
+                    </span>
+                </div>
             </template>
-
             <div>{{ report.record.description }}</div>
             <div v-for="(url, i) in attachmentURLs[index.toString()]" :key="i">
-                <!-- Image -->
+                <!-- Image with modal trigger -->
                 <img v-bind:src="url" alt="Image" style="width: 150px; padding: 5px;" data-bs-toggle="modal" data-bs-target="#imageModal" @click="modalImage = url">
+                <!-- Simple download link below the image -->
+                <a :href="url" download style="display: block; padding: 5px; text-align: center;">
+                    Download Image
+                </a>
             </div>
             <div style="font-size: small;">{{ new Date(report.timestamp) }}</div>
         </div>
     </div>
-        <!-- The Modal to Enlarge Image -->
-        <div class="modal fade" id="imageModal" tabindex="-1" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-lg">
-                 <div class="modal-content">
-                        <img v-bind:src="modalImage" alt="Image" class="img-fluid">
-                </div>
+    <!-- The Modal to Enlarge Image -->
+    <div class="modal fade" id="imageModal" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+            <div class="modal-content">
+                <img v-bind:src="modalImage" alt="Image" class="img-fluid">
             </div>
         </div>
+    </div>
 </template>
+
+
 
 <script>
 import { getProvenance, getAttachment } from '~/services/azureFuncs';
@@ -109,4 +114,8 @@ export default {
   border-radius: 5px;
   font-size: 14px;
 }
+
+
+
+
 </style>
