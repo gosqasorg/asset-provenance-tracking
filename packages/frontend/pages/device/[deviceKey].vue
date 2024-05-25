6ac2fe861d1e909c2dec6a2ef9f34c4e9f819996
judithweng <83525666+judithweng@users.noreply.github.com>
2024-05-24 22:24:34 -0400

    change color, fonts, and style to match style guide (#149)

--- packages/frontend/pages/device/[deviceKey].vue
+++ packages/frontend/pages/device/[deviceKey].vue
@@ -4,26 +4,72 @@ const deviceKey = route.params.deviceKey;
 </script>
 
 <template>
-  <div>
-    <h1><div>Device Key: {{ route.params.deviceKey }}</div></h1>
-    <!-- TODO: We might want to call this an Admin key if it has a reporting key -->
-    <div v-if="!isLoading">
-      <div>Name: {{deviceRecord.deviceName}}</div>
+    <nav class="navbar navbar-expand-md navbar-light bg-sky">
+    <div class="container">
+        <a href="/" class="navbar-brand">
+            <img src="../../assets/styles/gosqas_logo.png" width="300">
+        </a>
+
+        <!-- toggle button for mobile -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="main-nav" 
+        aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button> 
+
+        <div class="collapse navbar-collapse justify-content-end align-center " id="main-nav">
+            <ul class="navbar-nav ms-2">
+                <li class="nav-item pe-4">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
     </div>
-    <div><a :href="`/provenance/${route.params.deviceKey}`">View History Records</a></div>
-    <div><GenerateQRCode :deviceKey="route.params.deviceKey"></GenerateQRCode></div>
+    </nav>
+
+  <div class="container-md my-4">
+    <div class="row justify-content-between">
+        <div class="col-sm-6 col-lg-9">
+
+            <div class="mt-4 mb-2 text-iris fs-1" v-if="!isLoading">{{deviceRecord.deviceName}}</div>
+            <!-- TODO: We might want to call this an Admin key if it has a reporting key -->
+        
+            <div>Device Key: {{ route.params.deviceKey }}</div>
+            <div class="my-2" v-if="!isLoading">{{deviceRecord.description}}</div>
+        
+            <div> 
+                <button class="btn mt-1 bg-iris text-white me-4 px-4"><a :href="`/provenance/${route.params.deviceKey}`" style="color: white; text-decoration: none">View Provenance Records</a></button>
+                <button class="btn mt-1 bg-sky px-5">Download QR Code</button>
+            </div>
+
+        </div>
+        <div class="col-sm-6 col-lg-3 mt-2">
+
+            <div><GenerateQRCode :deviceKey="route.params.deviceKey"></GenerateQRCode></div>
+
+        </div>
+
+    </div>
+
+
+    <!-- <div><a :href="`/provenance/${route.params.deviceKey}`">View History Records</a></div> -->
+
+
     <!--Put the Reporting Key here if there is one -->
     <div v-if="!isLoading">
-    <div v-if="hasReportingKey">
-    Reporting Key:
-    <div>
-    <a :href="`/provenance/${deviceRecord.reportingKey}`">{{deviceRecord.reportingKey}}</a>
+        <div v-if="hasReportingKey" class="mt-4 mb-2 text-iris fs-2">
+        Reporting Key:
+        </div>
+            <div>
+                <a :href="`/provenance/${deviceRecord.reportingKey}`">{{deviceRecord.reportingKey}}</a>
+            </div>
     </div>
-      </div>
-    </div>
+
     <!--Put the Child List key here if there are any -->
-    Child Keys:
-<div>
+    <div v-if="childKeys" class="mt-4 mb-2 text-iris fs-3"> ChildKeys: </div>
+    <div> 
     <KeyList v-bind:keys="childKeys"/>
     </div>
   </div>
