7f905667b63450bd40d72912b8aa2cfc5f036b70
anusha <anushashringi@gmail.com>
2024-06-15 10:57:24 -0700

    testing

--- packages/frontend/utils/colorTag.ts
+++ packages/frontend/utils/colorTag.ts
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-
+// TAKE THIS LINE OUT 
 // Array of 100 random colors codes.
 const colors = [
   "#e9347b",
