9ca9ba8d5ec8061b869c825a697e1448d88ddb13
anusha <anushashringi@gmail.com>
2024-06-16 11:54:09 -0700

    anusha commentary license

--- packages/frontend/components/CreateContainer.vue
+++ packages/frontend/components/CreateContainer.vue
@@ -1,3 +1,17 @@
+<!-- CreateContainer.vue -- Creation of Container
+Copyright (C) 2024 GOSQAS Team
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
 <template>
     <form enctype="multipart/form-data" class="bg-sky p-3" @submit.prevent="submitForm">
         <p class="text-iris mt-1">Create New Container</p>
