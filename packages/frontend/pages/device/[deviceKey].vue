f4e11ecbbe7358c12e06f1502fd9f72d9a4d1d6f
K42IE <katiepryal@live.com>
2024-06-17 15:17:24 -0700

    Title bar is now a default layout (on all pages) (#169)
    * Inital commit
    
    * Made title-bar a default layout (also if you click the logoit goes to the main landing page)

--- packages/frontend/pages/device/[deviceKey].vue
+++ packages/frontend/pages/device/[deviceKey].vue
@@ -4,31 +4,6 @@ const deviceKey = route.params.deviceKey;
 </script>
 
 <template>
-    <nav class="navbar navbar-expand-md navbar-light bg-sky">
-    <div class="container">
-        <a href="/" class="navbar-brand">
-            <img src="../../assets/styles/gosqas_logo.png" width="300">
-        </a>
-
-        <!-- toggle button for mobile -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="main-nav" 
-        aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button> 
-
-        <div class="collapse navbar-collapse justify-content-end align-center " id="main-nav">
-            <ul class="navbar-nav ms-2">
-                <li class="nav-item pe-4">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-    </nav>
-
   <div class="container-md my-4" v-if="!isLoading">
     <div class="row justify-content-between">
         <div class="col-sm-6 col-lg-9">
