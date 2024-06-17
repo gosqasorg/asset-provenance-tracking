c365bb41ec84176aa71d8702c0b2fe6fea0e244f
K42IE <katiepryal@live.com>
2024-06-17 15:17:24 -0700

    Title bar is now a default layout (on all pages) (#169)
    * Inital commit
    
    * Made title-bar a default layout (also if you click the logoit goes to the main landing page)

---
+++ packages/frontend/layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-md navbar-light bg-sky">
+            <div class="container">
+                <a href="/" class="navbar-brand">
+                    <img src="../assets/styles/gosqas_logo.png" width="300">
+                </a>
+
+                <!-- toggle button for mobile -->
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="main-nav" 
+                aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button> 
+
+                <div class="collapse navbar-collapse justify-content-end align-center " id="main-nav">
+                    <ul class="navbar-nav ms-2">
+                        <li class="nav-item pe-4">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div>
+            <slot/>
+        </div>
+    </div>
+</template>
