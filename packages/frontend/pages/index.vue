ba5853be96daa94818cf14ec05b0d56b040d7114
anushashringi042 <152938229+anushashringi042@users.noreply.github.com>
2024-07-15 20:22:32 -0700

    Adding AGPL License Commentary  #160  (#165)
    * testing
    
    * anusha commentary license
    
    * Deploy Front End  (#154)
    
    * remove frontend folder from workspaces
    add azure static web app config + deploy instructions
    
    * mroe readme
    
    * add link to azure SWA CLI
    
    * update package lock
    
    * Add GH action
    
    * add azure/static-web-apps-cli dev dependency
    
    * add deploy to github workflow (commented out temporarily)
    
    ---------
    
    Co-authored-by: Harry Pierson <harrypierson@outlook.com>
    
    * small change to trigger github action
    
    * move GH workflow file to correct folder
    
    * fix GH workflow
    
    * trigger wf
    
    * tweak wf
    
    * I can't believe I forgot the checkout action 🤦‍♂️
    
    * fix upload wf step
    
    * deploy frontend GH WF step
    
    * node 18
    
    * only run frontend deployment wf for frontend changes
    
    * Title bar is now a default layout (on all pages) (#169)
    
    * Inital commit
    
    * Made title-bar a default layout (also if you click the logoit goes to the main landing page)
    
    * Fix warnings (#174)
    
    * Inital commit
    
    * inital commit
    
    * inital commit (#184)
    
    * Fixed textTooLink warnings (#175)
    
    * Inital commit
    
    * inital commit
    
    * clean up
    
    * Nav-bar Formating Update (#187)
    
    * initial commit
    
    * fix for extra padding
    
    * Fixed Device Page Text Styling  (#194)
    
    * Fixed Device Page Text Styles
    
    * Fixed Device Page Text Styles
    
    * create error when adding a child that does not exist (#162)
    
    * changing function names
    
    * checking if entered child key exists
    
    * provenance records can be broken down into different lines now
    
    * removing textToLink class
    
    * fixing spacing
    
    * Update main page to match style guide (#183)
    
    * moving create forms into a separate folder
    
    * input file button colors
    
    * change form bg colors to lighter blue (sky to frost)
    
    * change font color on input text placeholders:
    
    * changing borders
    
    * remove borders
    
    * children device name input box styled accordingly
    
    * clean up code
    
    * adding tag input field
    
    * notify all checkbox
    
    * clean code
    
    * Edited format a little to fit Figma
    
    * fix warning about notifyAll
    
    ---------
    
    Co-authored-by: K42IE <katiepryal@live.com>
    
    * Fix direct navigation to device page  (#195)
    
    * changes fix the error on npm run preview but fail on npm run dev
    
    * restore original
    
    * fork qrcode-vue3, change files locally
    
    * adding a submodule
    
    * pulling changes from qrcode submodule
    
    * moving all files from fork to fully local
    
    * renaming folder
    
    ---------
    
    Co-authored-by: Harry Pierson <harrypierson@hotmail.com>
    Co-authored-by: Harry Pierson <harrypierson@outlook.com>
    Co-authored-by: K42IE <katiepryal@live.com>
    Co-authored-by: judithweng <83525666+judithweng@users.noreply.github.com>
    Co-authored-by: Robert L. Read <read.robert@gmail.com>

--- packages/frontend/pages/index.vue
+++ packages/frontend/pages/index.vue
@@ -1,3 +1,17 @@
+<!-- index.vue -- adjusting container
+Copyright (C) 2024 GOSQAS Team
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
 <!--
     This is the landing page where you can create a new device to track
 -->
