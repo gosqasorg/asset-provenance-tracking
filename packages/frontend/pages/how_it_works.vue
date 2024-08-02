d8f81a2a4c72a10b000ec11fdcd4ad27f4cf3c31
judithweng <83525666+judithweng@users.noreply.github.com>
2024-08-02 15:26:39 -0400

    basics, need to fix spacing and size

--- packages/frontend/pages/how_it_works.vue
+++ packages/frontend/pages/how_it_works.vue
@@ -15,6 +15,71 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
 -->
 
 <template>
-    Testing
+    <div class="container-fluid" style="padding: 80px 200px 100px 200px;"> 
+        <div class="row" >
+            <h1 class="text-iris" style="margin-bottom:32px;">How It Works</h1>
+            <p>Designed by Global Open Source Quality Assurance System (GOSQAS), 
+                Global Distributed Tracking (GDT) is a software platform enabling closed-loop tracking for products, 
+                information, and logistics.<br><br>
+
+                Our development process is led by the mission of “Trust Through Transparency”. We've optimized Global 
+                Distributed Tracking to function in low-resource environments where internet and electricity access is limited.</p>
+
+            <ul class="text-iris" style="font-size: 20px; font-weight: 600;">Global Distributed Tracking targets solutions in:</ul>
+            <div class="col-6 mx-3">
+                <li>Last mile verification</li>
+                <li>Fraud and theft prevention</li>
+            </div>            
+            <div class="col">
+                <li>Quality Assurance</li>
+                <li>Donor/NGO Communication</li>
+            </div>
+
+            <ul class="text-iris" style="font-size: 20px; font-weight: 600;">Features include:</ul>
+            <div class="col-6 mx-3">
+                <li>Completely free service with no hidden fees</li>
+                <li>Fully encrypted data</li>
+                <li>Open source transparency</li>
+            </div>            
+            <div class="col">
+                <li>No downloads</li>
+                <li>No-hassle security</li>
+                <li>Tamper-proof, append-only records</li>
+            </div>
+
+        </div>
+
+        <div class="row">
+            <h2 class="text-iris">Overview</h2>
+            <div class="col text-center">
+                <img src="../assets/images/cross-icon.png" style="height:50px">
+                <p>1. Manufacturer creates a GDT identifier to share with testing agency</p>
+            </div>
+            <div class="col text-center">
+                <img src="../assets/images/upload-icon.png" style="height:44.86px">
+                <p>2. Testing agency uploads documentation</p>
+            </div>
+            <div class="col text-center">
+                <img src="../assets/images/check-icon.png" style="height:50px">
+                <p>3. Distributor views information within GDT and verifies product</p>
+            </div>
+            <div class="col text-center">
+                <img src="../assets/images/gosqas-icon.png" style="height:50px">
+                <p>4. Medical staff received verified product and records shipment receipt in GDT</p>
+            </div>
+
+        </div>
+
+
+
+    </div>
 
 </template>
+
+<style>
+
+li{
+    font-weight: 400;
+}
+
+</style>
