c91871778a95b1d05ecbbcba0d395583881af65b
judithweng <83525666+judithweng@users.noreply.github.com>
2024-08-03 14:03:36 -0400

    fixing spacing and format, need to fix mobile version

--- packages/frontend/pages/how_it_works.vue
+++ packages/frontend/pages/how_it_works.vue
@@ -14,10 +14,14 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
     This is the How it Works page for GOSQAS
 -->
 
+<script setup lang="ts">
+    const route = useRoute()
+</script>
+
 <template>
-    <div class="container-fluid" style="padding: 80px 200px 100px 200px;"> 
-        <div class="row" >
-            <h1 class="text-iris" style="margin-bottom:32px;">How It Works</h1>
+    <div class="container-fluid" id="how-it-works-container"> 
+        <h1 class="text-iris" style="margin-bottom:32px;">How It Works</h1>
+        <div class="row" id="how-it-works">
             <p>Designed by Global Open Source Quality Assurance System (GOSQAS), 
                 Global Distributed Tracking (GDT) is a software platform enabling closed-loop tracking for products, 
                 information, and logistics.<br><br>
@@ -25,61 +29,102 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
                 Our development process is led by the mission of “Trust Through Transparency”. We've optimized Global 
                 Distributed Tracking to function in low-resource environments where internet and electricity access is limited.</p>
 
-            <ul class="text-iris" style="font-size: 20px; font-weight: 600;">Global Distributed Tracking targets solutions in:</ul>
-            <div class="col-6 mx-3">
-                <li>Last mile verification</li>
-                <li>Fraud and theft prevention</li>
-            </div>            
-            <div class="col">
-                <li>Quality Assurance</li>
-                <li>Donor/NGO Communication</li>
+            <div class="row" id="targets">
+                <ul class="text-iris" style="font-size: 20px; font-weight: 600;">Global Distributed Tracking targets solutions in:</ul>
+                <div class="col mx-2">
+                    <li>Last mile verification</li>
+                    <li>Fraud and theft prevention</li>
+                </div>            
+                <div class="col">
+                    <li>Quality Assurance</li>
+                    <li>Donor/NGO Communication</li>
+                </div>
             </div>
 
-            <ul class="text-iris" style="font-size: 20px; font-weight: 600;">Features include:</ul>
-            <div class="col-6 mx-3">
-                <li>Completely free service with no hidden fees</li>
-                <li>Fully encrypted data</li>
-                <li>Open source transparency</li>
-            </div>            
-            <div class="col">
-                <li>No downloads</li>
-                <li>No-hassle security</li>
-                <li>Tamper-proof, append-only records</li>
+            <div class="row" id="features">
+                <ul class="text-iris" style="font-size: 20px; font-weight: 600;">Features include:</ul>
+                <div class="col mx-2">
+                    <li>Completely free service with no hidden fees</li>
+                    <li>Fully encrypted data</li>
+                    <li>Open source transparency</li>
+                </div>            
+                <div class="col">
+                    <li>No downloads</li>
+                    <li>No-hassle security</li>
+                    <li>Tamper-proof, append-only records</li>
+                </div>
             </div>
 
         </div>
 
-        <div class="row">
+        <div class="row" id="overview">
             <h2 class="text-iris">Overview</h2>
-            <div class="col text-center">
-                <img src="../assets/images/cross-icon.png" style="height:50px">
+            <div class="col text-center" id="overview-cols">
+                <img src="../assets/images/cross-icon.png" style="height:50px; margin-bottom:20px">
                 <p>1. Manufacturer creates a GDT identifier to share with testing agency</p>
             </div>
-            <div class="col text-center">
-                <img src="../assets/images/upload-icon.png" style="height:44.86px">
+            <div class="col text-center" id="overview-cols">
+                <img src="../assets/images/upload-icon.png" style="height:40px; margin-bottom:30px">
                 <p>2. Testing agency uploads documentation</p>
             </div>
-            <div class="col text-center">
-                <img src="../assets/images/check-icon.png" style="height:50px">
+            <div class="col text-center" id="overview-cols">
+                <img src="../assets/images/check-icon.png" style="height:50px; margin-bottom:20px">
                 <p>3. Distributor views information within GDT and verifies product</p>
             </div>
-            <div class="col text-center">
-                <img src="../assets/images/gosqas-icon.png" style="height:50px">
+            <div class="col text-center" id="overview-cols">
+                <img src="../assets/images/gosqas-icon.png" style="height:55px; margin-bottom:15px">
                 <p>4. Medical staff received verified product and records shipment receipt in GDT</p>
             </div>
 
         </div>
 
-
-
+        <Learn_more id="learn"></Learn_more>
     </div>
+
 
 </template>
 
-<style>
+<script lang="ts">
+import Learn_more from '~/layouts/learn_more.vue';
+</script>
+
+<style scoped>
 
 li{
     font-weight: 400;
+    text-indent: -20px;
+    margin-left: 20px;
+}
+
+
+/* For screens smaller than 768px */
+@media (max-width: 768px) {
+    #how-it-works-container{
+        padding: 20px 20px 40px 20px;
+        gap: 48px;
+    }
+    #overview-cols{
+        min-width: 390px;
+    }
+}
+
+/* For screens larger than 768px */
+@media (min-width: 768px) {
+    #how-it-works-container{
+        padding: 80px 200px 100px 200px;
+        gap:48px;
+    }
+    #how-it-works{
+        gap: 20px;
+    }
+    #overview{
+        margin-top:48px;
+        gap:20px;
+        
+    }
+
+    
+
 }
 
 </style>
