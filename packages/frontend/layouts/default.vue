ef28a4fb0649b4e3ea2774c9644a8d691268be99
judithweng <wengjudith@gmail.com>
2024-08-30 18:20:54 -0400

    mobile menu nav bar

--- packages/frontend/layouts/default.vue
+++ packages/frontend/layouts/default.vue
@@ -1,15 +1,14 @@
 <template>
     <div>
-        <nav class="navbar navbar-expand-md navbar-light bg-frost custom-nav">
+        <!-- <nav class="navbar navbar-expand-md navbar-light bg-frost custom-nav">
             <div class="container-fluid p-0">
                 <a href="/" class="navbar-brand" >
                     <img src="../assets/styles/gosqas_logo.png" height="42px">
-                </a>
+                </a> -->
  
  
                 <!-- toggle button for mobile -->
-                <button class="navbar-toggler border-0 custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="main-nav"
-                aria-expanded="false" aria-label="Toggle navigation">
+                <!-- <button class="navbar-toggler border-0 custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav">
                     <span class="navbar-toggler-icon"></span>
                 </button>
  
@@ -28,7 +27,51 @@
                     </ul>
                 </div>
             </div>
+        </nav> -->
+
+        <nav class="navbar navbar-expand-md bg-frost">
+            <div class="container-fluid">
+                <a href="/" class="navbar-brand" >
+                    <img src="../assets/styles/gosqas_logo.png" height="42px" style="margin: 10px;">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav5"
+                    aria-controls="navbarNav5" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav5">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/about">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/how_it_works">How It Works</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Data & Privacy</a>
+                        </li>
+                        <div class="mobile-nav">
+                            <li>
+                                <a class="nav-link" href="#">GDT GitHub</a>
+                            </li>
+                            <li>
+                                <a class="nav-link" href="#">Technical Documents</a>
+                            </li>
+                        </div>
+                        <span>
+                            <ButtonComponent buttonText="Track an Asset"></ButtonComponent>
+                            <div class="mobile-nav">
+                                    <ButtonComponent buttonText="Create a Device" backgroundColor="#e6f6ff" color="#4e3681"></ButtonComponent>
+                            </div>
+                        </span>
+                    </ul>
+                </div>
+            </div>
         </nav>
+
+
+
+
+
         <div>
             <slot/>
         </div>
@@ -39,51 +82,52 @@
  
 <style scoped>
 
-/* Styling for toggle bar */
-.custom-toggler .navbar-toggler-icon {
-    font-size: 30px;
-    background-image: url(
- "data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(30, 32, 25, 1)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E");
-}
- 
-.navbar-toggler:focus {
-    outline: none;
-    box-shadow: none;
-}
- 
- 
-@media (max-width:420px) {
-    /* Dynamically resize image + toggle past this point*/
-    img {
-        max-width: 100%;
-        height: auto;
-    }
- 
-    .navbar-brand {
-        width: 70%;
-    }
- 
-    .navbar-toggler-icon {
-        max-width: 100%;
-    }
- 
-    .custom-toggler {
-        width: 20%;
-    }
- }
- 
 
-.custom-nav { /* mobile view */
-    padding: 0px 7px 0px 20px;
-    height: 101px;
+nav{
+    font-size: 20px;
+    font-weight: 400;
+    color: #1E2019;
 }
- 
- 
-@media (min-width: 768px) { /* desktop view */
-  .custom-nav {
-    padding: 0px 30px 0px 30px;
-    height: 101px;
+
+.navbar-collapse {
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 2; /* This is 2 because the LargeTogle.vue is 1 */
   }
+
+.navbar-toggler,
+.navbar-brand {
+  z-index: 3;
 }
- 
+
+.navbar-toggler:focus {
+    box-shadow: none;
+  }
+
+
+.navbar-collapse .nav-link:hover,
+.navbar-collapse .nav-link.active {
+    color: #4e3681;
+}
+
+.navbar-collapse.show {
+    transition: all 0.5s;
+    height: 100vh;
+
+}
+
+@media (max-width: 768px) {
+    .mobile-nav {
+        display: inline-block;
+    }
+}
+
+@media (min-width: 768px) {
+    .mobile-nav {
+        display: none;
+    }
+}
+
+
 </style>
