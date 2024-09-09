555504e6ef73a3a8bed11bbce7fbb08f355e2bd9
judithweng <83525666+judithweng@users.noreply.github.com>
2024-09-09 18:59:15 -0400

    'Download Image' and Long Words Issue in Provenance Feed (#258)
    * word break to fit box
    
    * make images only available for preview
    
    ---------
    
    Co-authored-by: Robert L. Read <read.robert@gmail.com>

--- packages/frontend/components/Provenance/Feed.vue
+++ packages/frontend/components/Provenance/Feed.vue
@@ -39,9 +39,11 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
             
             <div v-for="(attachment, i) in attachmentURLs[index.toString()]" :key="i">
                 <!-- Image -->
-                <img :src="attachment.url" :alt="Image" style="width: 150px; padding: 5px;" data-bs-toggle="modal" data-bs-target="#imageModal" @click="modalImage = attachment.url">
+                 <div v-if="((attachment.fileName).toLowerCase()).endsWith('.jpg') || ((attachment.fileName).toLowerCase()).endsWith('.png')  ">
+                    <img :src="attachment.url" :alt="Image" style="width: 150px; padding: 5px;" data-bs-toggle="modal" data-bs-target="#imageModal" @click="modalImage = attachment.url">
+                 </div>
                 <a :href="attachment.url" :download="attachment.fileName" style="display: block; padding: 5px; text-align: left;">
-                    Download Image
+                    Download File
                 </a>
             </div>
             
@@ -126,6 +128,8 @@ export default {
   padding: 20px;
   margin-bottom: 14px;
   border-radius: 20px;
+  width: 70%; /* Assuming the width is to fill the container */
+  word-wrap: break-word;
 
 }
 .tag-container {
