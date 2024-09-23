4510d0f0cd711ef5b087a8a87e0412bd43c6ee28
Ben Coombs <bencoombs@protonmail.com>
2024-09-24 00:23:33 +1200

    Remove dummy tests.

--- packages/backend/test/backend.spec.ts
+++ packages/backend/test/backend.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-import { toHex, fromHex, decodeKey, calculateDeviceID } from "../src/functions/httpTrigger";
+import { toHex, decodeKey } from "../src/functions/httpTrigger";
 
 describe("Test algorithms", () => {
     it("should decode the key", () => {
@@ -7,18 +7,5 @@ describe("Test algorithms", () => {
         const key = decodeKey(deviceKey);
         expect(toHex(key)).toEqual("2311c8c1bbb6bd7b4198c6e47711bb96");
     });
-
-    it("should calculate the device key as a string", async () => {
-        const deviceKey = "5LAtuNjm3iuAR3ohpjTMy7";
-        const id = await calculateDeviceID(deviceKey);
-        expect(id).toEqual(id);
-    });
-
-    it("should calculate the device key as binary", async () => {
-        const deviceKey = "5LAtuNjm3iuAR3ohpjTMy7";
-        const id = await calculateDeviceID(fromHex(deviceKey));
-        expect(id).toEqual(id);
-    });
-
 });
 
