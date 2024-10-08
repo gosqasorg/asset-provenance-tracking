f915ac376e32d6febd02e41425d2233f17588ca6
Nora Moor <135470174+noramoor2007@users.noreply.github.com>
2024-10-07 20:43:14 -0400

    Update Feed.vue

--- packages/frontend/components/Provenance/Feed.vue
+++ packages/frontend/components/Provenance/Feed.vue
@@ -39,10 +39,6 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
             
             <div v-for="(attachment, i) in attachmentURLs[index.toString()]" :key="i">
                 <!-- Image -->
-                 <!--THIS WAS THE ISSUE:-->
-                 <!-- <div v-if="((attachment.fileName).toLowerCase()).endsWith('.jpg') || ((attachment.fileName).toLowerCase()).endsWith('.png')  ">
-                    <img :src="attachment.url" :alt="Image" style="width: 150px; padding: 5px;" data-bs-toggle="modal" data-bs-target="#imageModal" @click="modalImage = attachment.url">
-                </div> -->
                 <img :src="attachment.url" :alt="Image" style="width: 150px; padding: 5px;">
                 <a :href="attachment.url" :download="attachment.fileName" style="display: block; padding: 5px; text-align: left;">
                     Download File
