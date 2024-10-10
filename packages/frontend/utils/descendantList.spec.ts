318b1c1b321c58275a50a18fbc5f27471a86c828
Ben Coombs <bencoombs@protonmail.com>
2024-10-11 10:30:14 +1300

    Child keys not rendering correctly (#306)
    * Move child key search logic out of vue page and add unit tests.
    
    * Update provenance page to use helper functions.
    
    * Hide child key text.
    
    * Cleanup.
    
    * Handle undefined provenance on the priority notice.

---
+++ packages/frontend/utils/descendantList.spec.ts
@@ -0,0 +1,19 @@
+import { describe, it, expect } from 'vitest';
+import { getChildKeys } from './descendantList';
+import { fakeProvenanceNoChildren, fakeProvenanceWithChildren } from '~/test/data/provenance';
+
+describe('descendantList', () => {
+    it('should return an empty list of keys', async () => {
+        const keys = getChildKeys(fakeProvenanceNoChildren)
+        expect(keys).toEqual([]);
+    });
+
+    it('should return an list of child keys', async () => {
+        const keys = getChildKeys(fakeProvenanceWithChildren)
+        const expected = [
+            ["childkey1"],
+            ["childkey2", "childkey3"]
+        ];
+        expect(keys).toEqual(expected);
+    });
+});
