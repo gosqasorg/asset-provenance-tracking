0c501287c2f4c51e484bd8a08e2892c00c98e334
Melanie Laporte <107901386+melanielaporte@users.noreply.github.com>
2024-10-14 20:46:18 -0500

    Test push to repository (#318)

--- packages/frontend/pages/about.vue
+++ packages/frontend/pages/about.vue
@@ -1,5 +1,5 @@
 <!-- about.vue
-Copyright (C) 2024 GOSQAS
+Copyright (C) 2024 GOSQAS Test
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
 published by the Free Software Foundation, either version 3 of the
