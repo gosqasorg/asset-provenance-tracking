c3c58f4806d39ba7db4c584e55ae23c52700640d
Ben Coombs <bencoombs@protonmail.com>
2024-11-06 12:24:53 +1300

    Simplify CreateRecord component.

--- packages/frontend/components/Provenance/CreateRecord.vue
+++ packages/frontend/components/Provenance/CreateRecord.vue
@@ -14,37 +14,28 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
 
-<!--
-    This component is a form. The form is used to create a new device that we will track the
-    providence for.
-    Resourses:
-    https://test-utils.vuejs.org/guide/essentials/forms
--->
 
 <template>
     <form enctype="multipart/form-data" class='bg-frost' @submit.prevent="submitForm">
       <h5 class="text-iris">Create New History Record</h5>
       <div>
-        <input type="text" class="form-control" name="description" id="provenance-description" v-model="description" placeholder="History Description" />
-        <input type="text" class="form-control" name="container-key" id="container-key" v-model="containerKey" placeholder="Container Key (optional)"/>
-        <input type="text" class="form-control" name="children-key" id="children-key" v-model="childKeys" placeholder="Contained Device Keys (optional, separated with a coma)"/>
-        <div>
-            <span v-for="(childkey1, index) in childKeys" :key="childkey1">
-                {{ childkey1 }}{{ index !== childKeys.length - 1 && childkey1.endsWith(',') ? ' ' : ''}}
+        <input type="text" class="form-control" name="description" id="provenance-description" v-model="record.description" placeholder="History Description" />
+        <!-- <input type="text" class="form-control" name="container-key" id="container-key" v-model="containerKey" placeholder="Container Key (optional)"/> -->
+        <!-- <input type="text" class="form-control" name="children-key" id="children-key" v-model="record.children_key" placeholder="Contained Device Keys (optional, separated with a coma)"/> -->
+        <!-- <div>
+            <span v-for="(childkey1, index) in record.children_key" :key="childkey1">
+                {{ childkey1 }}{{ index !== record.children_key.length - 1 && childkey1.endsWith(',') ? ' ' : ''}}
             </span>
-        </div>
+        </div> -->
         <div>
             <h5 class="text-iris">Device Image (optional)    </h5>
             <input type="file" class="form-control" accept="*" @change="onFileChange" capture="environment" multiple />
         </div>
         <h5 class="text-iris">Add Tags (optional)</h5>
-        <ProvenanceTagInput class="form-control" id="provenanceTag" v-model="tags" @updateTags="handleUpdateTags" placeholder="Device tag"/>
+        <ProvenanceTagInput class="form-control" id="provenanceTag" v-model="record.tags" @updateTags="handleUpdateTags" placeholder="Device tag"/>
         <div>
-            <span v-for="(tag, index) in tags" :key="tag">{{ tag }}{{ index !== tags.length - 1 ? ', ' : '' }} </span>
+            <span v-for="(tag, index) in record.tags" :key="tag">{{ tag }}{{ index !== record.tags.length - 1 ? ', ' : '' }} </span>
         </div>
-        <h5 class="text-iris">
-            <input type="checkbox" class="form-check-input" id="notify-all"/> Notify all Children?
-        </h5>
     </div>
     <div class="d-grid" id="submit-button">
         <button-component buttonText="Create History Record" type="submit" />
@@ -53,22 +44,16 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
 </template>
 
 <script lang="ts">
-import { postProvenance, getProvenance } from '~/services/azureFuncs';
+import { postProvenance } from '~/services/azureFuncs';
 import { EventBus } from '~/utils/event-bus';
-import { getAllDescendants } from '~/utils/descendantList';
-
+import { ProvenanceRecord } from '~/utils/types';
 
 export default {
 
     data() {
         return {
-            description: '',
+            record: new ProvenanceRecord(),
             attachments: [] as File[] | null,
-            tags: [] as string[],
-            containerKey: '',
-            childKeys: [] as string[],
-            hasParent: false,
-            isReportingKey: false,
         }
     },
     props: {
@@ -78,20 +63,16 @@ export default {
             required: true,
         },
         deviceRecord: {
-            // type: Any, // TODO: add type
-            default: null,
+            type: Object, // ProvenanceRecord,//Any, // TODO: add type
+            default: new ProvenanceRecord(),
             required: true,
         },
     },
     computed: {
-        uniqueChildKeys() {
-            const uniqueValues = [...new Set(this.childKeys)];
-            return uniqueValues.filter(childKey => childKey); // Filter out empty strings if any
-        }
     },
     methods: {
         handleUpdateTags(tags: string[]) {
-            this.tags = tags;
+            this.record.tags = tags;
         },
         onFileChange(e: Event) {
             const target = e.target as HTMLInputElement;
@@ -101,145 +82,30 @@ export default {
             }
         },
         refresh() {
-            this.description = '';
+            this.record = new ProvenanceRecord();
             this.attachments = null;
-            this.tags = [];
-            this.containerKey = '';
-            this.childKeys = [];
-            this.hasParent = false;
-            this.isReportingKey = false;
-        },
-        async messageChildren(childrenkeys: string[],recallReason: string, tags: string[]) {
-            for (const key of childrenkeys) {
-                if (key != "" && key != "undefined") {
-                    postProvenance(key,
-                    {
-                        blobType: 'deviceRecord',
-                        description: recallReason,
-                        children_key: '',
-                        tags: tags,
-                    },
-                    this.attachments || [])
-                }
-            }
         },
         async submitRecord() {
-            const response = await getProvenance(this.deviceKey);
+            // const provenance = await getProvenance(this.deviceKey);
             
-            // Split into useful information that we will use later.
-            // Metadata is stored in the first record.
-            let local_deviceRecord = response[0].record;
-            this.hasParent = local_deviceRecord.hasParent;
-            this.isReportingKey = local_deviceRecord.isReportingKey;
-            let descendantsList = await getAllDescendants(this.deviceKey);
-
-            let parentResponse;
-            // Create a container (group) key if the conditions are met,
-            // ie. the key is the top level element.
-            if (validateKey(this.containerKey)) {
-                if (!this.hasParent && !descendantsList.includes(this.containerKey)) {
-                    parentResponse = await postProvenance(this.containerKey, {
-                        blobType: 'deviceRecord',
-                        description: this.description, // keep the same description?
-                        tags: [],
-                        children_key: [this.deviceKey], // Add the device key as a child.
-                        hasParent: true,
-                    }, this.attachments || [])
-
-                    this.hasParent = true;
-                } else {
-                    console.log("This device already has a container.");
-                    this.description = this.description + "\nError: Container could not be added.";
-                }
+            // let deviceCreationRecord, deviceRecord, allOtherRecords;
+            // ({ deviceCreationRecord, deviceRecord, allOtherRecords } = decomposeProvenance(provenance));
+            
+            if (this.record.children_key) {
+                await addChildKeys(this.deviceKey, this.record.children_key, this.record.description || '', []);
             }
 
-            const a = JSON.parse(parentResponse?.record)
-            addChildKeys()
-
-            // Add child keys (if any).
-            // 1 is used because the deviceKey is already added (TODO: consider creating that here).
-            if (this.childKeys.length > 1) {
-                let string_children = this.childKeys.toString();
-                let entered_children = string_children.split(",");
-                entered_children = [...new Set(entered_children)]; //removing any duplicates
-                let new_children_list = entered_children.slice(0); //copy this exact array
-                let childExists, child_prov;
-                for (let i of entered_children) {
-                    let index = new_children_list.lastIndexOf(i);
-
-                    //First, check if entered child exists
-                    try { 
-                        const response = await getProvenance(i);
-                        child_prov = response;
-                        childExists = true;
-                    } catch(error) {
-                        new_children_list.splice(index, 1);
-                        this.description = this.description + `\nError: Entered child key does not exist.`;
-                        childExists = false;
-                    }
-
-                    // If entered child exist, check if it has a parent or is already a descendant of this device
-                    if(childExists) {
-                        const child_record = child_prov[0].record;
-                        
-                        if (child_record.hasParent) { // Child has a parent, cannot be added
-                            this.description = this.description + `\nError: Entered child key already has a container.`;
-                            new_children_list.splice(index, 1);
-                        } else {
-                            let descendants = await getAllDescendants(i);
-                            if (descendants.includes(this.deviceKey)) { // Device is a descendant of entered child, cannot be added
-                                this.description = this.description + `\nError: Child device could not be added.`;
-                                new_children_list.splice(index, 1);
-                            } else {
-                                postProvenance(i,
-                                    {
-                                        blobType: 'deviceRecord',
-                                        description: "Added parent", // need to discuss whether we want to have a unique description
-                                        tags: [],
-                                        children_key: [],
-                                        hasParent: true,  // make sure the child has parent = true
-                                    },
-                                    this.attachments || []
-                                )
-                            }
-                        }
-                    }                        
-                }
-                this.childKeys = new_children_list;
-            } 
-
-            //
-            const recall = this.tags.indexOf("recall", 0);        
-
-            // "recall" is being added....
-            if (recall > -1 || (<HTMLInputElement>document.getElementById("notify-all")).checked) {
-                let reason = ""
-                if (recall > -1) { 
-                    reason = "Recalled by Admin Key";
-                } else { 
-                    reason = this.description; 
-                    this.tags = (this.tags).concat(['notify_all']);
-                }
-
-                if (this.isReportingKey) {
-                    // reporting keys do not have the ability to recall
-                    console.log("Action failed. This is a reporting key.");
-                } else {
-                    await this.messageChildren(descendantsList, reason, this.tags)
-                    console.log("Finished recalling/informing");
-                }
-            }
-            
+            // await notifyAll(this.record.tags);
+           
             try {
-                // Here we post the povenance itself...
                 await postProvenance(this.deviceKey,
                     {
                         blobType: 'deviceRecord',
-                        description: this.description,
-                        tags: this.tags,
-                        children_key: this.childKeys,
-                        hasParent: this.hasParent,
-                    },
+                        description: this.record.description,
+                        tags: this.record.tags,
+                        children_key: this.record.children_key,
+                        hasParent: this.record.hasParent,
+                    } as ProvenanceRecord,
                     this.attachments || []
                 )
                 
@@ -254,7 +120,7 @@ export default {
         },
         async submitForm() {
             await this.submitRecord()
-            window.location.reload(); //once they submit it just reloads the entire page.
+            // window.location.reload();
         },
     }
 };
