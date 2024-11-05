12344a63b0d464dd21c1fd11cdc9dc42cfd0009d
Ben Coombs <bencoombs@protonmail.com>
2024-11-06 12:23:24 +1300

    Move logic out of components.

--- packages/frontend/utils/descendantList.ts
+++ packages/frontend/utils/descendantList.ts
@@ -2,38 +2,8 @@
 // devices (children, grandchildren, great granchildren, so on) given a key
 // or the list of direct descendants (children only) given a key 
 
-import { getProvenance } from '~/services/azureFuncs';
-import type { Provenance } from '~/utils/types';
-
-// gets all children given a key
-export async function getChildrenKeys(key: string) {
-    const response = await getProvenance(key);
-    return getChildKeys(response);
-}
-
-// Gets all descendants given a key
-export async function getAllDescendants(key: string) {
-
-    let children_list = await getChildrenKeys(key);
-
-    for (let child_key of children_list) {
-        children_list = children_list.concat(await getAllDescendants(child_key));
-    }
-
-    return children_list;
-}
-
-
-export function decomposeProvenance(provenance: Provenance[]) {
-    // Decompose the provenance array into the first entry and the remaining entries.
-
-    const provenanceNoRecord = provenance.slice(0, -1); // get the provenance without device creation
-    const deviceCreationRecord = [provenance.at(-1)]; // the last record in the list should be the device creation
-    const deviceRecord = provenance[provenance.length - 1].record; // subset of the record for rendering
-
-    return { provenanceNoRecord, deviceCreationRecord, deviceRecord };
-}
-
+import { getProvenance, postProvenance } from '~/services/azureFuncs';
+import type { Provenance, ProvenanceRecord } from '~/utils/types';
 
 export function getChildKeys(provenance: Provenance[]): string[] {
     let childKeys: string[] = []
@@ -50,23 +20,116 @@ export function getChildKeys(provenance: Provenance[]): string[] {
     return childKeys;
 }
 
+export function decomposeProvenance(provenance: Provenance[]) {
+    if (provenance.length == 0) {
+        throw new Error("No provenance records found.");
+    }
+    // Decompose the provenance array into the first entry and the remaining entries.
+    const deviceCreationRecord = [provenance.at(-1)]; // the last record in the list should be the device creation
+    const deviceRecord: ProvenanceRecord = provenance[provenance.length - 1].record; // subset of the record for rendering
+    const allOtherRecords = provenance.slice(0, -1); // get the provenance without device creation
+
+    return { deviceCreationRecord, deviceRecord, allOtherRecords };
+}
+
+// Get all children given a key. Calls API.
+export async function getChildrenKeys(key: string) {
+    const response = await getProvenance(key);
+    return getChildKeys(response);
+}
+
+// Gets all descendants given a key
+export async function getAllDescendants(key: string) {
+    let children_list = await getChildrenKeys(key);
+
+    for (let child_key of children_list) {
+        // TODO: does this recursion work? It will get last children, not all children.
+        let children = await getAllDescendants(child_key);
+        children_list = children_list.concat(children);
+    }
+
+    return children_list;
+}
+
 export function deduplicateKeys(keys: string[]): string[] {
     return Array.from(new Set(keys))
 }
 
-export function addChildKeys(existing_keys: string[], new_keys: string[]) {
-    if (new_keys.length == 0) {
-        console.error("No child keys provided.");
+export async function discombobulate(response: { record: any; }[]) {
+    // Split into useful information that we will use later.
+    // Metadata is stored in the first record.
+    if (response.length == 0) {
+        throw new Error("No provenance records found.");
+    }
+    const localDeviceRecord = response[0].record;
+    const hasParent = localDeviceRecord.hasParent;
+    const isReportingKey = localDeviceRecord.isReportingKey;
+    return [ localDeviceRecord, hasParent, isReportingKey ];
+    // let descendantsList = await getAllDescendants(this.deviceKey);
+}
+
+
+export async function addChildKeys(deviceKey: string, childKeys: string[], description: string, attachments: File[]) {
+    // Add child keys (if any).
+    // 1 is used because the deviceKey is already added (TODO: consider creating that here).
+
+    if (!childKeys || childKeys.length == 0) {
+        console.log("No child keys to add.");
         return;
     }
 
-    for (const key in new_keys) {
-        if (!existing_keys.includes(key)) {
-            existing_keys.push(key)
-        }
-    }
-}
+    const dedupedKeys = deduplicateKeys(childKeys);
+    
+    let string_children = childKeys.toString();
+    let entered_children = string_children.split(",");
+    entered_children = [...new Set(entered_children)]; //removing any duplicates
 
+    let childrenToAdd = entered_children.slice(0); //copy this exact array
+
+    
+    let childProvenance;
+    for (let childKey of dedupedKeys) {
+        const index = childrenToAdd.lastIndexOf(childKey);
+
+        //First, check if entered child exists
+        try { 
+            childProvenance = await getProvenance(childKey);
+        } catch(error) {
+            childrenToAdd.splice(index, 1); // remove the child key from the list
+            description += `\nError: Entered child key does not exist.`;
+        }
+
+        // If entered child exist, check if it has a parent or is already a descendant of this device
+        if(childProvenance) {
+            const childRecord = childProvenance[0].record;
+            
+            if (childRecord.hasParent) {
+                description += `\nError: Entered child key already has a container.`;
+                childrenToAdd.splice(index, 1);
+            } else {
+                const descendants = await getAllDescendants(childKey);
+                if (descendants.includes(deviceKey)) {
+                    description += `\nError: Child device could not be added.`;
+                    childrenToAdd.splice(index, 1);
+                } else {
+                    // Add the parent to the child.
+                    await postProvenance(childKey,
+                        {
+                            blobType: 'deviceRecord',
+                            description: "Added parent",
+                            tags: [],
+                            children_key: [],
+                            hasParent: true,
+                        } as ProvenanceRecord,
+                        attachments || []
+                    )
+                }
+            }
+        }                        
+    }
+    // this.childKeys = childrenToAdd;
+    return  ;
+}
 
 // let string_children = this.childKeys.toString();
 // let entered_children = string_children.split(",");
