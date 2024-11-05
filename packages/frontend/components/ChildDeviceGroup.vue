00127805b1fd01a1001cb27ece75a43e1dfb1524
Ben Coombs <bencoombs@protonmail.com>
2024-11-05 13:00:40 +1300

    Add components.

---
+++ packages/frontend/components/ChildDeviceGroup.vue
@@ -0,0 +1,35 @@
+
+<template>
+   <ChildDeviceRow v-for="i in numChildren" :key="i" :name="bob" />
+
+    <!-- TODO: Paginate with a maximum of 10 children displayed -->
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            maxDisplayedChildren: 10
+        }
+    },
+    props: {
+        numChildren: {
+            type: Number,
+            required: true
+        }
+    },
+    computed: {
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+
+
+<style scoped>
+
+
+</style>
---
+++ packages/frontend/components/ChildDeviceRow.vue
@@ -0,0 +1,49 @@
+
+<template>
+   <!-- newInput = document.createElement('input');
+                    newInput.id = 'name-input-' + (i);
+                    newInput.type = 'text';
+                    newInput.style.border = "0px";
+                    newInput.style.borderRadius = "4px";
+                    newInput.style.margin = "3px"
+                    newInput.required = true;
+                    newLabel =  document.createElement('label');
+                    newLabel.textContent = 'Device #'+ (i+1) +' Name:  ';
+                    fieldset.appendChild(newLabel);
+                    fieldset.appendChild(newInput);
+                    fieldset.appendChild(document.createElement('br')); -->
+    <input v-bind:value="text" type="text" class="form-control" required placeholder="Device Name" style="margin-bottom: 0.25em;">
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            text: "hello2"
+        }
+    },
+    props: {
+        key: {
+            type: Number,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        }
+    },
+    computed: {
+    },
+    methods: {
+        
+    }
+    }
+</script>
+
+
+
+<style scoped>
+
+
+</style>
