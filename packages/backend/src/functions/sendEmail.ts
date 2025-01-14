22a0b9e652ebfa42e4858a913bd50ae4d08f5cc0
Ben Coombs <bencoombs@protonmail.com>
2025-01-14 14:26:20 +1300

    Cleanup.

--- packages/backend/src/functions/sendEmail.ts
+++ packages/backend/src/functions/sendEmail.ts
@@ -1,45 +1,42 @@
 import { EmailClient, KnownEmailSendStatus } from "@azure/communication-email";
 
-require("dotenv").config();
-
-// This code demonstrates how to fetch your connection string
-// from an environment variable.
-// Check the address to your address
 const connectionString = process.env['COMMUNICATION_SERVICES_CONNECTION_STRING'];
 const emailClient = new EmailClient(connectionString);
 
+// Send an email using the Azure Communication Services Email SDK
 export async function sendEmail(from_address: string, to_address: string, subject: string, plainText: string, displayName: string) {
+  if (!from_address || !to_address || !subject || !plainText || !displayName) {
+    throw "Missing required parameter(s).";
+  }
+
   const POLLER_WAIT_TIME = 10
   try {
     const message = {
-      senderAddress: from_address,//"donotreply@6104f838-923b-468b-a2cc-4d5aeaea891a.azurecomm.net",
+      senderAddress: from_address,
       content: {
-        subject: subject,//"Welcome to Azure Communication Services Email",
-        plainText: plainText,//"This is testing email sending function.",
+        subject: subject,
+        plainText: plainText,
       },
       recipients: {
         to: [
           {
-            address: to_address,// "input@gmail.com",
-            displayName: displayName,//"Customer Name",
+            address: to_address,
+            displayName: displayName,
           },
         ],
       },
     };
 
     console.log("Sending email...", message);
-
     const poller = await emailClient.beginSend(message);
 
     if (!poller.getOperationState().isStarted) {
       throw "Poller was not started."
     }
 
-    console.log("Sending...");
     let timeElapsed = 0;
     while (!poller.isDone()) {
       poller.poll();
-      console.log("Email send polling in progress");
 
       await new Promise(resolve => setTimeout(resolve, POLLER_WAIT_TIME * 1000));
       timeElapsed += 10;
