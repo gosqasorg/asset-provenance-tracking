ce62d322a85562b5ffc6f3bbd1762c7881540b41
agilpatr <agilpatr@uvm.edu>
2025-02-16 21:34:23 -0500

    Tag functionality works, but now have a bug that newly created provenance records can't be viewed.

--- packages/frontend/components/Provenance/TagInput.vue
+++ packages/frontend/components/Provenance/TagInput.vue
@@ -17,7 +17,10 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
 
 <template>
   <div class="container">
-    <input id="tagInputField" class="form-control" required placeholder="Record Tag" :value="editableValue" @input="onInput" />
+    <ul class="ulTagsList" id="ulTagsList"><input id="tagInputField2" type="text" class="form-control" placeholder="Record Tag" @input="updateTags" :value="editableValue" /></ul>
+
+    <!-- UI Toolkit (for x icon on tags) -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/thinline.css">
 
     <h5 class="mt-3 mb-1 text-iris">Suggested Tags</h5>
     <div class="tag-container mb-2">
@@ -30,6 +33,54 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
 <script>
 import { getDecipheredForbiddenTags } from '~/utils/forbiddenTags';
 import { TagName } from "~/utils/tags";
+
+let storedTags = [];  // all tags in input field inside bubbles
+let createdTags = [];  // all tags in input field
+
+function removeTag(tag) {
+  // Remove tag from screen and storedList
+  if (storedTags.includes(tag)) {
+    storedTags.forEach((item, index) => {
+      if (item === tag) {
+          storedTags.splice(index, 1);
+      }
+    });
+  }
+
+  // Reload tags
+  createTag();
+}
+
+function createTag() {
+  const ul = document.getElementById("ulTagsList");
+  let input = document.getElementById("tagInputField2");
+
+  // Load stored tags
+  ul.querySelectorAll("li").forEach(li => li.remove());
+  storedTags.slice().reverse().forEach(tag =>{
+    // Create new tag
+    let liTag = document.createElement('li');
+    liTag.style.color = textColorForTag(tag);
+    liTag.style.backgroundColor = getColorForTag(tag);
+    liTag.innerHTML = `${tag} <i class="uit uit-multiply"></i>`;
+    
+    // Create event listener for click
+    liTag.addEventListener('click', function() {
+      removeTag(tag);
+    });
+
+    let tagsUl = document.getElementById('ulTagsList');
+    if (tagsUl) {
+      tagsUl.insertAdjacentElement("afterbegin", liTag);
+    }
+  });
+
+  if (storedTags.length == 0) {
+    input.placeholder = "Record Tag";
+  } else {
+    input.placeholder = "";
+  }
+}
 
 export default {
   name: 'TagInput',
@@ -52,6 +103,7 @@ export default {
   computed: {
     editableValue: {
       get() {
+        // TODO: this.tags is only typed tags, might cause issues
         return this.tags.join(this.separator);
       },
       set(value) {
@@ -60,12 +112,20 @@ export default {
         const transformedValues = uniqueValues.map(tag => tag.toLowerCase().trim());
         this.tags = transformedValues;
         const cleanArray = this.cleanArray(this.tags); // Calling forbidden tags method.
-        this.$emit ('updateTags', cleanArray);
+
+        // TODO: Send stored tags (tags in input in bubbles) as well as typed tags
+        createdTags = [];
+        storedTags.forEach(tag => createdTags.push(tag));
+        cleanArray.forEach(tag => createdTags.push(tag));
+        console.log("NEW Tags Array:", createdTags)
+        console.log("Cleaned Tags:", cleanArray, "\n\n")
+
+        this.$emit ('updateTags', createdTags);
       },
     },
   },
   methods: {
-    cleanArray(arr) { //checking to see if correct
+    cleanArray(arr) { // checking to see if correct
         const forbiddenWords = getDecipheredForbiddenTags();
         const cleanedArray = arr.filter (tagName => !forbiddenWords.includes (tagName.toLowerCase ()));
         return cleanedArray;
@@ -74,9 +134,36 @@ export default {
       this.editableValue = event.target.value;
     },
     moveTagToForm(tag, event) {
-      if (document.getElementById('tagInputField')) {
-          const inputField = document.getElementById('tagInputField');
-          this.editableValue += tag + ' ';
+      // Store the value that was clicked
+      if (!storedTags.includes(tag)) {
+        storedTags.push(tag);
+      }
+
+      createdTags = [];
+      storedTags.forEach(tag => createdTags.push(tag));
+      this.$emit ('updateTags', createdTags);  // TODO: update tags in other files
+      createTag();
+    },
+    updateTags(event) {
+      let tag = event.target.value;
+
+      // Get the last char of a str, if it's a space then remove space and add to list
+      if (storedTags.includes(tag.substring(0, tag.length - 1)) || tag == ' ') {
+        event.target.value = "";
+      } else if (tag[tag.length - 1] == ' ') {
+        // Check to make sure the word is clean before creating tag
+        tag = tag.substring(0, tag.length - 1);
+        let cleanTag = this.cleanArray([tag]);
+        this.editableValue = '';
+        
+        if (tag == cleanTag[0]) {
+          storedTags.push(tag);
+          event.target.value = "";
+          createTag();
+        }
+      } else {
+        // Call set (which updates tags in other files) when not creating a new tag
+        this.editableValue = event.target.value;
       }
     },
   },
@@ -84,16 +171,11 @@ export default {
 </script>
 
 <style>
-  .tag-container {
-    display: flex;
-    flex-wrap: wrap;
-  }
-
   .tag {
     color: #333;
     padding: 5px 10px;
     margin: 5px;
-    border-radius: 5px;
+    border-radius: 15px;
     font-size: 14px;
     border: none;
   }
@@ -103,8 +185,35 @@ export default {
     padding: 0px;
   }
 
-  #editableValue {
-    margin-top: 5px;
-    padding-top: 5px;
+  input:focus {
+    box-shadow: none !important;
+  }
+
+  .content ul.ulTagsList li i{
+    font-size: 14px;
+    cursor: pointer;
+  }
+
+  .content ul.ulTagsList{
+    display: flex;
+    flex-wrap: wrap;
+    margin: 12px 0px 12px 0px;
+    padding-left: 0px;
+    border-radius: 5px;
+    background-color: white;
+  }
+
+  .content ul.ulTagsList li{
+    font-size: 14px;
+    margin: 4px 3px;
+    list-style: none;
+    border-radius: 15px;
+    padding: 5px 8px 5px 10px;
+  }
+
+  .content ul.ulTagsList input{
+    flex: 1;
+    border: none;
+    outline: none;
   }
 </style>
