3d41dd707134bab6f1267a0db7d02ecd0a7ae4bf
Robert L. Read <read.robert@gmail.com>
2025-05-29 22:54:02 -0500

    repairing deployment script

--- deploy_test_fe.sh
+++ deploy_test_fe.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 # deploy_test_fe.sh
 # set top level variables describing how/where function app is deployed
-export LOCATION=eastus2
+export LOCATION=westus2
 export RG_NAME=rg-gdt-test
 export STG_NAME=gdtteststorage
 export AZURE_STORAGE_ACCOUNT_NAME=gdtteststorage
 # Run this command to get the account storage key
 # az storage account keys list  --account-name $STG_NAME --resource-group $RG_NAME | jq '.[0].value'
 export AZURE_STORAGE_ACCOUNT_KEY=
-export FUNC_NAME=gdttestbackend
-export MY_STATIC_WEB_APP_NAME=gdt-test-frontend
+# Note: This is really the name of the function, which serves as the backend
+export FUNC_NAME=gosqasbe
+export MY_STATIC_WEB_APP_NAME=gosqas-frontend
 
-export BACKEND_URL=https://gdttestbackend.azurewebsites.net/api
+export BACKEND_URL=https://gosqasbe.azurewebsites.net/api
+# export FRONTEND_URL=https://red-stone-00f5d251e.5.azurestaticapps.net/
 export FRONTEND_URL=https://gosqas.org
 
 # Deploy frontend
 cd packages/frontend
 npx nuxi build
-swa deploy .output/public/ --app-name=$MY_STATIC_WEB_APP_NAME --no-use-keychain --env test
+# swa deploy .output/public/ --app-name=$MY_STATIC_WEB_APP_NAME --no-use-keychain --env test
+swa deploy .output/public/ --app-name=$MY_STATIC_WEB_APP_NAME --no-use-keychain --env production
