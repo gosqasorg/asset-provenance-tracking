9f0a6bca99dbac1183c3833070bc4adf6aae98b3
Robert L. Read <read.robert@gmail.com>
2025-06-06 01:07:19 -0500

    Issue 449 launch video (#463)
    * modifications to main home page; mainly works but css not cleaned up, not tested in dark mode, and not as fluent as I would like.
    
    * home page mostly works
    
    * some improvements in fluency
    
    * videos on the how-it-works
    
    * donate page improved
    
    * adding a DMDM page
    
    * tweak
    
    * improving dark mode on how-it-works
    
    * adding dmdm video

---
+++ packages/frontend/pages/dmdm.vue
@@ -0,0 +1,115 @@
+<!-- dmdm.vue
+Copyright (C) 2025 GOSQAS
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
+<!--
+    This is the Decentralized Medical Device Manufacturing (DMDM) page for GOSQAS
+-->
+<script setup lang="ts">
+    const route = useRoute()
+</script>
+
+
+<template>
+    <div class="container-fluid" id="dmdm-container">
+        <h1>Making a Difference with DMDM</h1>
+        <div class="row">
+            <p style="font-weight: 400; margin-bottom: 30px;" >
+    Manufacturing open source medical hardware at scale, while meeting quality assurance standards,
+    can sometimes be a mysterious process to the outside observer. Through GOSQAS-sponsored
+    pilot program with Decentralized Medical Device Manufacturing (DMDM), this process is
+    becoming more tranparent and accessible.
+            </p>
+        </div>
+
+        <div class="row"  style="font-weight: 400;">
+    <h3>Open Source:</h3>
+            <div class="flexcontainer">
+<ul>
+  <li>Design</li>
+  <li>QMS</li>
+</ul>
+ <ul>
+  <li>Product Testing</li>
+  <li>Logistics Management</li>
+</ul>
+    </div>
+        </div>
+
+    <iframe src="https://player.vimeo.com/video/1086646484?h=f0cbc7fd6d&amp;title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+                        style="width: 100%; height: 540px;"
+    frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media" title="How to Create a Group Record with Decentralized Medical Device Manufacturing (DMDM)"></iframe>
+
+
+    </div>
+</template>
+
+
+
+<style scoped>
+
+.flexcontainer {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;       /* Allows wrapping onto a new line */
+  align-items: stretch;
+  justify-content: space-between;
+}
+
+
+/* For screens smaller than 768px */
+@media (max-width: 768px) {
+    #dmdm-container{
+        padding: 20px 20px 40px 20px;
+    }
+    .row{
+        margin-top:32px;
+    }
+}
+
+/* For screens larger than 768px */
+@media (min-width: 768px) {
+    #dmdm-container{
+        padding: 80px 200px 100px 200px;
+    }
+    .row{
+        margin-top:32px;
+    }
+}
+
+/* Dark mode version*/
+@media (prefers-color-scheme: dark) {
+    #dmdm-container {
+        background-color: #1E2019
+    }
+    h1,h3 {
+        color: #CCECFD;
+    }
+
+    p,li {
+        color: #FFFFFF;
+    }
+}
+/* Light mode version*/
+@media (prefers-color-scheme: light) {
+    #dmdm-container {
+        background-color: #FFFFFF;
+    }
+    h1,h3 {
+        color: #4E3681;
+    }
+    p,li {
+        color: #1E2019;
+    }
+}
+</style>
--- packages/frontend/pages/index.vue
+++ packages/frontend/pages/index.vue
@@ -21,8 +21,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
 
 <template>
     <div class="container-fluid">
-
-        <div class="row" id="first-row">
+        <div class="row odd-stripe" id="first-row">
             <div class="col-12 col-md-7" id="first-row-col">
                 <div class="row"> <h1 class="dark-mode-heading">Trust and transparency when you need it most.</h1> </div>
                 <div class="row"> <h4 class="dark-mode-heading">Explore Global Distributed Tracking (GDT), our open source software enabling closed-loop tracking for products, information, and logistics.</h4> </div>
@@ -42,16 +41,24 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
                     </div>
                     <div v-else style="padding-bottom: 20px;"></div>
                 </div>
-
             </div>
-
         </div>
 
-        <div class="row" id="second-row" >
+
+        <div class="row second-row" >
+          <h3>Global Distributed Tracking</h3>
+          <p style="font-weight: 400;">The Global Open Soruce Quality Assurance System proudly presents Global Distributed Tracking (GDT)&mdash;a free and open-source tracking platform. By reducing fraud, theft, counterfeiting, and lost shipments with secure encryption and a simple user interface, GDT helps create trust through transparency for your organization. </p>
+               <iframe src="https://player.vimeo.com/video/1083699280?h=941a4ccf67&amp;title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+                       style="width: 100%; height: 480px;"
+                       frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media" title="Global Distributed Tracking">
+               </iframe>
+        </div>
+
+        <div class="row odd-stripe"  >
             <div class="row rowtest">
                 <div class="col wrap-word" id="second-row-cols" v-for="item in second_row">
                     <h3 id="second-row-cols-h3">{{item.title}}</h3>
-                    <p style="font-weight: 400;">{{ item.descr }}</p>         
+                    <p style="font-weight: 400;">{{ item.descr }}</p>
                 </div>
             </div>
             <div class="col" style="text-align: center; margin-top: 50px;">
@@ -68,7 +75,34 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
               </button></RouterLink>
             </div>
 
-        </div> 
+        </div>
+
+        <div class="row second-row" >
+          <div class="dmdmcontainer">
+            <img src="/dmdmCropped.png" alt="Logo" class="dmdmimage" />
+            <div id="flexright">
+              <h3>Decentralized Medical Device Manufacturing (DMDM)</h3>
+              <p class="text" style="font-weight: 400;">Inspired by the need for life-saving tourniquets in Sudan and Gaza,
+  the FDA-registered Decentralized Medical Device Manufacturing (DMDM) team in Arizona piloted Global Distributed Tracking as an essential tool in their open-source manufacturing process.
+  Explore how the DMDM and GOSQAS partnership expands open-source technology access
+  for medical device compliance and distribution.
+              </p>
+            </div>
+          </div>
+          <div class="col" style="text-align: center; margin-top: 50px;">
+          <RouterLink to="/about"><button class="baseButton button" id="about-button" style="
+                  border-width: 2px;
+                  border-style: solid;
+                  padding: 10px 20px;
+                  margin: 0px;
+                  font-size: 20px;
+                  border-radius: 10px;
+                "
+              >
+               View our pilot program
+          </button></RouterLink>
+          </div>
+        </div>
 
         <Learn_more id="learn-more"></Learn_more>
 
@@ -101,6 +135,30 @@ export default {
 
 <style scoped>
 
+.dmdmcontainer {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;       /* Allows wrapping onto a new line */
+align-items: stretch;
+justify-content: space-around;
+}
+
+.dmdmimage {
+  height: auto;
+  max-height: 100%;
+  flex: 0 0 auto;         /* Don’t shrink, don’t grow */
+  max-width: 12em;    /* Optional: size constraint */
+  object-fit: contain;
+}
+
+#flexright {
+max-width: 70%;
+}
+
+.text-block {
+  flex: 1;              /* Let the text block take remaining space */
+}
+
 #wrap-word {
     overflow-wrap: break-word;
 }
@@ -120,7 +178,7 @@ export default {
     background-size: 60%;
 
     height: auto;
-    display: flex;  
+    display: flex;
     flex-wrap: wrap;
 }
 
@@ -131,6 +189,10 @@ export default {
 /* For screens less than 991px resize hand logo*/
 @media (max-width: 991px) {
     #first-row {
+        background-size: 50% !important;
+        height: auto;
+    }
+    .odd-stripe {
         background-size: 50% !important;
         height: auto;
     }
@@ -149,7 +211,7 @@ export default {
     #first-row-col{
         padding: 41px 33px 80px 33px;
     }
-    #second-row{
+    .second-row{
         padding: 40px 30px;
     }
     #second-row-cols{
@@ -172,7 +234,7 @@ export default {
         width: 180px !important;
     }
     #createRecordButton {
-        order: 1; 
+        order: 1;
     }
 }
 
@@ -189,7 +251,11 @@ export default {
     #first-row-col{
         padding: 60px 78px;
     }
-    #second-row {
+    .odd-stripe {
+        padding: 60px 78px;
+    }
+
+    .second-row {
         padding: 70px 100px;
     }
     #second-row-cols{
@@ -201,7 +267,7 @@ export default {
     .button{
         font-size: 20px;
         padding: 1px;
-        
+
     }
 
 }
@@ -217,9 +283,9 @@ export default {
   .dark-mode-heading {
     color: #FFFFFF;
   }
-  #first-row {
+  .odd-stripe{
         background-color: #1E2019;
-    }
+  }
 }
 
 /* Dark mode version of second row */
@@ -227,15 +293,17 @@ export default {
     #first-row {
         background-image: url(../assets/images/darkmode-hand.png);
     }
-    #second-row{
+    .second-row{
             background-color: #4E3681;
-        }
+    }
+    .odd-stripe{
+    }
 
     h3 {
-        color: #FFFFFF; 
+        color: #FFFFFF;
     }
     p {
-        color: #E6F6FF; 
+        color: #E6F6FF;
     }
     #about-button {
         color: #FFFFFF;
@@ -248,15 +316,15 @@ export default {
     #first-row {
         background-image: url(../assets/images/hand-icon.png);
     }
-    #second-row {
+    .second-row {
             background-color: #E6F6FF;
         }
 
     h3 {
-        color: #4E3681; 
+        color: #4E3681;
     }
     p {
-        color: #322253; 
+        color: #322253;
     }
     #about-button {
         color: #322253;
