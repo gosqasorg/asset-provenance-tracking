77c108f261b5379539139f81cdd5e47a8bbfbd76
Hieu Ngo <71876066+HieuNgoGH@users.noreply.github.com>
2025-08-20 15:50:37 -0700

    517 make description text field default to show 3 lines of text (#551)
    * changed description box to have 3 lines with scroll bar for overflow
    
    * commit with dos and polling python files
    
    * fixed minor typed error
    
    * fixed type error and added file for commit
    
    * deleted python scripts folder in 517
    
    * added CreateContainer and CreateRecord files
    
    * removed border highlight and changed corner radius

--- packages/frontend/components/Forms/CreateDevice.vue
+++ packages/frontend/components/Forms/CreateDevice.vue
@@ -26,8 +26,8 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>. -->
         <h4 class="mt-1 mb-3">Create New Record</h4>
 
         <div>
-            <input type="text" class="form-control" v-model="name" required placeholder="Record Title" maxlength="500">
-            <input type="text" class="form-control mt-3" v-model="description" required placeholder="Record Description" maxlength="5000">
+            <input type="text" class="form-control" v-model="name" required placeholder="Record Title" maxlength="500">  
+            <textarea id="record-description" v-model="description" required placeholder="Record Description" maxlength="5000" rows="3"></textarea>
             <div style="display: block;">
                 <h4 class="mt-3 mb-3">Record Image (optional)</h4>
                 <input type="file"  class="form-control" accept="*" @change="onFileChange" capture="environment" multiple />
@@ -178,6 +178,23 @@ export default {
 
     }
     
+    #record-description {
+        padding: 5px;
+        margin: 5px;
+        display: flex;
+        margin-left: auto;
+        margin-right:auto;
+        border-radius: 7px;
+        width: 100%;
+        outline: none;
+        border: none;
+        padding-left: 14px;
+    }
+
+    #record-description::placeholder{
+        color: black;
+    }
+
     #device-form > * {
         padding: 5px;
         margin: 5px;
@@ -241,3 +258,4 @@ export default {
 }
 
 </style>
+
