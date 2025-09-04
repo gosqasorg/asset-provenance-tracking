a199d97564ae5cb5622d733598e01aa342022e9c
Vincent Steffens <VinceSteffens@gmail.com>
2025-09-03 22:19:54 -0700

    Revert "Set coverage checks to below current levels"
    This reverts commit 0d798b30b3306359409ffa4228bbd8619f554e43.

--- packages/frontend/vitest.config.ts
+++ packages/frontend/vitest.config.ts
@@ -3,22 +3,5 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 export default defineVitestConfig({
   test: {
     environment: 'nuxt',
-
-    watch: false,
-    forceExit: true,
-    testTimeout: 10000,
-
-    clearMocks: true,
-    restoreMocks: true,
-
-    coverage: {
-        provider: 'v8',
-        thresholds: {
-            statements: 4.01,
-            branches:   30.01,
-            functions:  10.01,
-            lines:      4.01,
-        },
-    },
   }
 })
