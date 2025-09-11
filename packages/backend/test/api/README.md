c2ce3471339c09682f0265be0a1a703ceb94aa22
Vincent Steffens <VinceSteffens@gmail.com>
2025-09-10 17:08:09 -0700

    Infrastructure: smoketest

---
+++ packages/backend/test/api/README.md
@@ -0,0 +1,12 @@
+This directory is intended to house tests that test the API
+    from the user's perspective, against
+    the live-running, deployed API
+    on Red Stone.
+
+Tests will be run by 
+
+cd packages/backend
+npm run api_integration_tests
+
+Note that tab-to-autocomplete works with npm run, so:
+npm run a tab
---
+++ packages/backend/test/api/apiIntegrationTestsSmoketests.test.ts
@@ -0,0 +1,28 @@
+import { describe, it, expect } from "vitest";
+
+/* README: To add a test, add inside the global describe an additional it. For example:
+
+describe("Group of tests", () => {
+	it("Brief description that this tests foo", () => {
+		var val = do_thing();
+		expect(val).toBe(0);
+	});
+
+	it("Brief description that this tests bar", () => {
+		// structured similar to above
+	});
+
+	it("Another test", () => {
+		// More test contents
+	});
+
+	// More tests
+})
+
+*/
+
+describe("Tests-Selftest-Smoketest", () => {
+	it("Can the tests run?", () => {
+		expect(0).toBe(0);
+	});
+});
