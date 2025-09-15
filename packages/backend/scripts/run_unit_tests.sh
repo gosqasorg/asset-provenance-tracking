e07350f6de86f709b39c5f8f723dae0a025b833c
Vincent Steffens <paulErdos@users.noreply.github.com>
2025-09-15 03:38:44 -0700

    604 feature request create first basic read operation api integration test (#628)
    * Infrastructure: smoketest
    
    * Infrastructure: files, placeholders, guides
    
    * Add integration test construction walkthrough
    
    * Finalize test + test automation
    
    * Test automation attempt 2: add timeout
    
    * Add Documentation
    
    * Organize Backend API Code
    
    * Writeup Documentation + HowTo Guide
    
    * Fixed unit tests
    
    * Alter how tests are kicked off; organize tests
    
    * Cleanup: Update CI/CD Test Kickoff

--- packages/backend/scripts/run_unit_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Get azure default env vars
-eval $(cat local.settings.json | grep -i azure | egrep -vi 'comment|index' | sed 's,^  *,,' | sed 's,",,g' | sed 's/,//' | sed 's,: ,=,' | sed "s,=,=\'," | sed "s,$,',"  | sed 's,^,export ,')
-
-# Run unit tests: tests/functions/httptrigger.endpoints.spec.ts
-npx vitest run test/functions/httpTrigger.endpoints.spec.ts
-
