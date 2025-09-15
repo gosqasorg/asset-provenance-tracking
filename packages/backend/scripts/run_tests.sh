77d8de62944afbe45af48dfac790c8349d039826
Vincent Steffens <paulErdos@users.noreply.github.com>
2025-09-15 04:08:49 -0700

    Revert "604 feature request create first basic read operation api integration…"
    This reverts commit ee073510245cdb493054841cb4209246ac16e9c5.

---
+++ packages/backend/scripts/run_tests.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Get azure default env vars
+eval $(cat local.settings.json | grep -i azure | egrep -vi 'comment|index' | sed 's,^  *,,' | sed 's,",,g' | sed 's/,//' | sed 's,: ,=,' | sed "s,=,=\'," | sed "s,$,',"  | sed 's,^,export ,')
+
+
+# Run integration tests: tests/functions/httptrigger.endpoints.spec.ts
+npx vitest run test/functions/httpTrigger.endpoints.spec.ts
+
+
+# NOTE: Below currently fails (TypeError: fetch failed)
+# Methods that failed: put/getProvenanceRecords, getStatistics
+
+# Run unit tests: test/test.ts
+# npx tsx ./test/integrationTests.ts --put
+# npx tsx ./test/integrationTests.ts
+# npx tsx ./test/integrationTests.ts --statistics
+
