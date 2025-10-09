931791c7ddf41d33b8f7d9fd88ce6293a7e39908
Vincent Steffens <VinceSteffens@gmail.com>
2025-10-09 01:05:17 -0700

    Correct git

--- packages/backend/test/IntegrationTests/Live/create.test.ts
+++ packages/backend/test/IntegrationTests/Live/create.test.ts
@@ -48,8 +48,7 @@ describe("Group Creation Tests", () => {
 describe("Record Creation Tests", () => {
 	const baseUrl = 'https://gdtprodbackend.azurewebsites.net/api/provenance/'
 
-	// The most basic possible test -- create a record
-	it("(Smoketest) Create the most basic record", async () => {
+	it("Smoketest", async () => {
 		// Create record key
 		const deviceKey = await makeEncodedDeviceKey();
 		console.log("(1st Test) Created Device Key: " + deviceKey);
@@ -103,15 +102,62 @@ describe("Record Creation Tests", () => {
 		}
 	});
 
-	
-	// Most basic + one feature
-	it("", async() => {
+
+	it("Create a record with tags", async() => {
+		const deviceKey = await makeEncodedDeviceKey();
+		console.log("(2nd Test) Created Device Key: " + deviceKey);
+		let fullUrl = `${baseUrl}${deviceKey}`
+		expect(deviceKey.length).toBe(22);
+		expect(validateKey(deviceKey)).toBe(true);
+
+		// POST
+		try {
+			const data = {
+				blobType: 'deviceInitializer',
+				deviceName: "Create Record Test + 1 Feature",
+				description: "An API smoketest for creating a record with tags",
+				tags: ['smoketest', 'api'],
+				children_key: '',
+				hasParent: false,
+				isReportingKey: false,
+			}
+			const formData = new FormData();
+			formData.append("provenanceRecord", JSON.stringify(data));
+
+			const postResponse = await fetch(fullUrl, {
+				method: "POST",
+				body: formData,
+			});
+
+			expect(postResponse.ok).toBe(true);
+
+		} catch (error) {
+			console.error("(Create POST Test) Error creating a record: " + error); 
+			throw error;
+		}
+
+		// GET
+		let getResponse; 
+		try {
+			getResponse = await fetch(fullUrl);
+			getResponse = await getResponse.json();
+			let responseString = JSON.parse(JSON.stringify(getResponse[0]));
+
+			expect(JSON.stringify(getResponse)).not.toBe('[]');
+			expect(responseString.record.deviceName).toBe('Create Record Test + 1 Feature');
+			expect(responseString.record.description).toBe('An API smoketest for creating a record with tags');
+			expect(responseString.record.tags.length).toBe(2);
+			expect(JSON.stringify(responseString.record.tags)).toBe('["smoketest","api"]');
+
+		} catch(error) {
+			console.error('(Create GET Test) Failed to fetch url: ' + fullUrl + '\nError: ' + error) 
+			throw error;
+		}
 	});
 
 
     // Note: As we add feature tests, we'll accumulate them into the feature-complete test
-	// Everything all at once -- create a record with tags and an image
-	it("(Smoketest) Create a record with tags", async() => {
+	it("Feature-Complete Test", async() => {
 		const deviceKey = await makeEncodedDeviceKey();
 		console.log("(2nd Test) Created Device Key: " + deviceKey);
 		let fullUrl = `${baseUrl}${deviceKey}`
