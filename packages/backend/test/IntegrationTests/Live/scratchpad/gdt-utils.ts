c5705729e2e8ba666d70156a117424eb24a5556a
paulErdos <vincesteffens@gmail.com>
2025-11-02 22:38:42 -0800

    Creating records by api

--- packages/backend/test/IntegrationTests/Live/scratchpad/gdt-utils.ts
+++ packages/backend/test/IntegrationTests/Live/scratchpad/gdt-utils.ts
@@ -1,5 +1,8 @@
 #!/usr/bin/env -S npx --yes tsx
 
+//const baseUrl = "https://gosqasbe.azurewebsites.net/api/";
+const baseUrl = 'https://gdtprodbackend.azurewebsites.net/api/provenance/'
+
 export async function getNewDeviceKey() {
     const baseUrl = "https://gosqasbe.azurewebsites.net/api";
 
@@ -9,3 +12,65 @@ export async function getNewDeviceKey() {
     return deviceKey;
 }
 
+export async function createSimpleRecord(theName: string, theDescription: string): string {
+    const deviceKey = await getNewDeviceKey()
+    let fullUrl = `${baseUrl}${deviceKey}`
+
+    // POST record key
+    try {
+
+        // Can we leave these out and still read it in the frontend?
+        const data = {
+            //blobType: 'deviceInitializer',
+            deviceName: theName,
+            description: theDescription,
+            //tags: {},
+            //children_key: '',
+            //hasParent: false,
+            //isReportingKey: false,
+        }
+        const formData = new FormData();
+        formData.append("provenanceRecord", JSON.stringify(data));
+
+        const postResponse = await fetch(fullUrl, {
+            method: "POST",
+            body: formData,
+        });
+
+        if(! postResponse.ok) {
+            console.error('URL: ' + fullUrl)
+            console.error('Key: ' + deviceKey)
+            console.error(postResponse)
+            throw new Error(`Error: Record creation attempt failed with status ${postResponse.status}`)
+        }
+
+
+    } catch (error) {
+        console.error("(Create POST Test) Error creating a record: " + error); 
+        throw error;
+    }
+
+
+        /*
+    // GET record key to make sure it exists
+    let getResponse; 
+    try {
+        getResponse = await fetch(fullUrl);
+        getResponse = await getResponse.json();
+        let responseString = JSON.parse(JSON.stringify(getResponse[0]));
+        console.log(responseString)
+
+        expect(JSON.stringify(getResponse)).not.toBe('[]');
+        expect(responseString.record.deviceName).toBe('Create Record Test');
+        expect(responseString.record.description).toBe('An API smoketest for creating the most basic record');
+        expect(responseString.record.children_key).toBe("");
+        expect(responseString.record.hasParent).toBe(false);
+        expect(responseString.record.isReportingKey).toBe(false);
+    } catch(error) {
+        console.error('(Create GET Test) Failed to fetch url: ' + fullUrl + '\nError: ' + error) 
+        throw error;
+    }
+        */
+
+    return deviceKey;
+}
--- packages/backend/test/IntegrationTests/Live/scratchpad/gdt.ts
+++ packages/backend/test/IntegrationTests/Live/scratchpad/gdt.ts
@@ -1,57 +1,11 @@
 #!/usr/bin/env -S npx --yes tsx
 
-import { getNewDeviceKey } from './gdt-utils';
+import { 
+    getNewDeviceKey,
+    createSimpleRecord,
+} from './gdt-utils';
 
 console.log(await getNewDeviceKey())
+const simpleRecordId = await createSimpleRecord('Title', 'Description')
+console.log(simpleRecordId)
 
-/*
-
-const baseUrl = "https://gosqasbe.azurewebsites.net/api";
-        
-// Generate device keys in parallel
-const [deviceKeyResponse] = await Promise.all([
-    fetch(`${baseUrl}/getNewDeviceKey`),
-]);
-const deviceKey = await groupKeyResponse.text();
-console.log(deviceKey)  
-*/
-     
-
-/* 
-        // Create child and group records in parallel
-        const childFormData = new FormData();
-        childFormData.append("provenanceRecord", JSON.stringify({
-            blobType: "deviceInitializer",
-            deviceName: "child_smoketest",
-            description: "child for most basic smoketest group record creation",
-            tags: [],
-            children_key: "",
-            hasParent: false,
-            isReportingKey: false
-        }));
-        
-        const groupFormData = new FormData();
-        groupFormData.append("provenanceRecord", JSON.stringify({
-            blobType: "deviceInitializer",
-            deviceName: "group_smoketest",
-            description: "group for most basic smoketest group record creation",
-            tags: [],
-            children_key: [childKey],
-            hasParent: false,
-            isReportingKey: false
-        }));
-        
-        const [childResponse, groupResponse] = await Promise.all([
-            fetch(`${baseUrl}/provenance/${childKey}`, {
-                method: "POST",
-                body: childFormData,
-            }),
-            fetch(`${baseUrl}/provenance/${groupKey}`, {
-                method: "POST",
-                body: groupFormData,
-            })
-        ]);
-        
-        expect(childResponse.ok).toBe(true);
-        expect(groupResponse.ok).toBe(true);
-*/
