cfd0f999d9b4681ec1154957846a0cec3406f37c
Vincent Steffens <VinceSteffens@gmail.com>
2025-11-24 19:21:51 -0800

    Experiment: one should pass

--- packages/backend/test/IntegrationTests/Live/version.spec.ts
+++ packages/backend/test/IntegrationTests/Live/version.spec.ts
@@ -1,8 +1,6 @@
 import { describe, it, expect } from "vitest";
 import { TableClient, AzureNamedKeyCredential } from '@azure/data-tables';
 
-let timeout = 30000;  // Milliseconds; integration testing can be laggycd
-
 let accountName; if (isEmpty(accountName = process.env["AZURE_STORAGE_ACCOUNT_NAME"])) {
     throw new Error('Env vars not set')
 }
@@ -14,13 +12,14 @@ const baseUrl = accountName === "devstoreaccount1"
     ? `http://127.0.0.1:10000/devstoreaccount1`
     : `https://${accountName}.blob.core.windows.net`;
 
+let timeout = 30000;  // Milliseconds; integration testing can be laggycd
 
 let baseTestName: string;
 describe(baseTestName = "Tests for getVersion and setVersion", () => {
 
-	let thisTestName: string;
+    let thisTestName: string;
 
-	it(thisTestName = "API test for getVersion", async () => {
+    it(thisTestName = "API test for getVersion", async () => {
 
         // ---- Section 0/2: Setup --------------- // 
         const fullUrl = baseUrl + '/api/getVersion'
@@ -55,46 +54,10 @@ describe(baseTestName = "Tests for getVersion and setVersion", () => {
     
 
     }, timeout);
-thisTestName: string;
- 22
- 23     it(thisTestName = "API test for getVersion", async () => {
- 24
- 25         // ---- Section 0/2: Setup --------------- //
- 26         const fullUrl = baseUrl + '/api/getVersion'
- 27
- 28         // ---- Section 1/2: Invoking the API ---- //
- 29
- 30         let response
- 31         let responseCode;
- 32         let dataBody;
- 33         try {
- 34             response = await fetch(fullUrl).then(function(response){
- 35                 responseCode = response.status
- 36                 return response.text();
- 37             }).then(function(data){
- 38                 dataBody = data;
- 39             })
- 40             //console.log(response.body)
- 41         } catch(error) {
- 42             const testName = baseTestName + thisTestName;
- 43             const errorMessage = 'Failed to fetch (get) url: '
- 44             console.error(testName + errorMessage + fullUrl)
- 45             throw error;
- 46         }
- 47
- 48
- 49         // ---- Section 2/2: Tests -------------- //
- 50
- 51         expect(dataBody).toBe("44444")
- 52         expect(responseCode).toBe(200)
- 53
- 54     // ----------------------------------------------------------------------
- 55
- 56
- 57     }, timeout);
-	it(baseTestName = "API test for setVersion", async () => {
 
-		// ---- Section 0/2: Setup --------------- //
+    it(baseTestName = "API test for setVersion", async () => {
+
+        // ---- Section 0/2: Setup --------------- //
 
         const fullUrl = "http://localhost:7071/api/getVersion" 
 
@@ -157,6 +120,6 @@ thisTestName: string;
 
         expect(checkTestVersion).toBe(testVersion)
 
-	}, timeout);
+    }, timeout);
 
 })
