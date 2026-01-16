f53666ddf9d6c04e579b1cc7d8504e272cca8361
Khadijatou Dibba <kd2732@nyu.edu>
2026-01-15 17:47:46 -0800

    adding code to httpTrigger.ts

--- packages/backend/src/functions/httpTrigger.ts
+++ packages/backend/src/functions/httpTrigger.ts
@@ -599,9 +599,42 @@ export async function notifyChildren(request: HttpRequest, context: InvocationCo
     }
  }
 
+export async function postNotificationEmail(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    try{
+        const body = await request.json() as any;
+        const email = body.email;
+        const recordKey= body.recordKey;
+
+        if (!email || !recordKey){
+            return {
+                jsonBody: {error: "missing data"},
+                status: 400
+            }
+        }
+        console.log("Received signup for " + email)
+        return {
+            jsonBody: {message: "Success"},
+            status: 201
+        } 
+        
+    }catch(error){
+        console.error(error.message);
+        return {
+            jsonBody: {message: "Internal Server Error"},
+            status: 500,
+
+        }
+
+    }
+}
 
 /* ----- API Endpoints Section 2/2: Route Definitions ----- */
 
+app.post("postNotificationEmail", {
+    authLevel: 'anonymous',
+    route: 'ReceiveNotificationEmail',
+    handler: postNotificationEmail
+})
 app.get("getProvenance", {
     authLevel: 'anonymous',
     route: 'provenance/{deviceKey}',
