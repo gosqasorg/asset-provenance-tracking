5a18edb1ba375cb8410ce5b814792050c15c5cdf
Alison Gilpatrick <alisongilpatrick@icloud.com>
2026-02-25 15:13:01 -0500

    last formatting fix

--- packages/frontend/services/azureFuncs.ts
+++ packages/frontend/services/azureFuncs.ts
@@ -13,17 +13,17 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import { validateKey } from '~/utils/keyFuncs';
+import { validateKey } from "~/utils/keyFuncs";
 
 // method takes the base58 encoded device key
 export async function getProvenance(deviceKey: string) {
     try {
         if (!validateKey(deviceKey)) {
-            throw new Error('Bad key provided');
+            throw new Error("Bad key provided");
         }
-
+        
         const baseUrl = useRuntimeConfig().public.baseUrl;
-        const fullUrl = baseUrl + '/provenance/' + deviceKey;
+        const fullUrl = baseUrl + "/provenance/" + deviceKey;
 
         try {
             let response = await fetchUrl(fullUrl);
@@ -41,11 +41,11 @@ export async function getProvenance(deviceKey: string) {
 export async function getAttachment(baseUrl: string, deviceKey: string, attachmentID: string) {
     try {
         if (!validateKey(deviceKey)) {
-            throw new Error('Bad key provided.');
+            throw new Error("Bad key provided.");
         }
 
         const response = await fetch(`${baseUrl}/attachment/${deviceKey}/${attachmentID}`, {
-            method: 'GET'
+            method: "GET",
         });
 
         const blob = await response.blob();
@@ -56,7 +56,7 @@ export async function getAttachment(baseUrl: string, deviceKey: string, attachme
         if (!fileName) {
             // Fetch the attachment name
             const nameResponse = await fetch(`${baseUrl}/attachment/${deviceKey}/${attachmentID}/name`, {
-                method: 'GET'
+                method: "GET",
             });
             fileName = await nameResponse.text();
         }
@@ -64,25 +64,25 @@ export async function getAttachment(baseUrl: string, deviceKey: string, attachme
     } catch (error) {
         console.error('Error occurred during getAttachment request:', error);
         throw error; // re-throw the error if you want to handle it further up the call stack
-    }
+    }      
 }
 
 export async function postProvenance(deviceKey: string, record: any, attachments: readonly File[]) {
     if (!validateKey(deviceKey)) {
-        throw new Error('Bad key provided.');
+        throw new Error("Bad key provided.");
     }
 
     const baseUrl = useRuntimeConfig().public.baseUrl;
     const formData = new FormData();
-    formData.append('provenanceRecord', JSON.stringify(record));
+    formData.append("provenanceRecord", JSON.stringify(record));
     for (const blob of attachments) {
         formData.append(blob.name, blob);
     }
-
-    const fullUrl = baseUrl + '/provenance/' + deviceKey;
+    
+    const fullUrl = baseUrl + "/provenance/" + deviceKey;
     try {
         let response = await fetchUrl(fullUrl, formData);
-        return (await response.json()) as { record: string; attachments?: string[] };
+        return await response.json() as { record: string, attachments?: string[] };
     } catch (error) {
         throw error;
     }
@@ -91,24 +91,24 @@ export async function postProvenance(deviceKey: string, record: any, attachments
 export async function postEmail(email: string) {
     const baseUrl = useRuntimeConfig().public.baseUrl;
     const formData = new FormData();
-    formData.append('email', email);
+    formData.append("email", email);
 
-    // backend urls are converted to all lowercase on deployment
+                                            // backend urls are converted to all lowercase on deployment
     const response = await fetch(`${baseUrl}/feedbackvolunteer`, {
         method: 'POST',
-        body: formData
+        body: formData,
     });
     if (response.status != 200) {
-        throw new Error('postEmail: Failed to save email address');
+        throw new Error('postEmail: Failed to save email address')
     }
 }
 
 export async function getStatistics() {
     const baseUrl = useRuntimeConfig().public.baseUrl;
     const response = await fetch(`${baseUrl}/statistics`, {
-        method: 'GET'
+        method: "GET",
     });
-    return (await response.json()) as { record: string; timestamp: number }[];
+    return await response.json() as { record: string, timestamp: number }[];
 }
 
 async function fetchUrl(url: string, formData?: FormData) {
@@ -118,12 +118,12 @@ async function fetchUrl(url: string, formData?: FormData) {
         try {
             if (typeof formData !== 'undefined') {
                 response = await fetch(`${url}`, {
-                    method: 'POST',
-                    body: formData
+                    method: "POST",
+                    body: formData,
                 });
             } else {
                 response = await fetch(`${url}`, {
-                    method: 'GET'
+                    method: "GET"
                 });
             }
 
@@ -131,13 +131,13 @@ async function fetchUrl(url: string, formData?: FormData) {
                 return response;
             }
         } catch (e) {
-            console.log('Fetch attempt failed: ' + e);
+            console.log("Fetch attempt failed: " + e);
         }
     }
 
     if (response !== undefined && response.status !== 200) {
-        console.log(`Failed to post provenance: ${response.status} ${response.statusText}`);
-        throw new Error(response.status + ' ' + response.statusText);
+        console.log(`Failed to post provenance: ${response.status} ${response.statusText}`)
+        throw new Error(response.status + " " + response.statusText)
     } else {
         throw new Error(`Could not connect to the server, check your internet connection and try again`);
     }
