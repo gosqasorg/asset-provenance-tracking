41debf5f7d7ab8f15ee3888b02931d137ac38e74
Alison Gilpatrick <alisongilpatrick@icloud.com>
2026-02-25 13:22:39 -0500

    update spacing

--- packages/frontend/services/azureFuncs.ts
+++ packages/frontend/services/azureFuncs.ts
@@ -17,153 +17,149 @@ import { validateKey } from '~/utils/keyFuncs';
 
 // method takes the base58 encoded device key
 export async function getProvenance(deviceKey: string) {
-  try {
-    if (!validateKey(deviceKey)) {
-      throw new Error('Bad key provided');
-    }
+	try {
+		if (!validateKey(deviceKey)) {
+			throw new Error('Bad key provided');
+		}
 
-    const baseUrl = useRuntimeConfig().public.baseUrl;
-    const fullUrl = baseUrl + '/provenance/' + deviceKey;
+		const baseUrl = useRuntimeConfig().public.baseUrl;
+		const fullUrl = baseUrl + '/provenance/' + deviceKey;
 
-    try {
-      let response = await fetchUrl(fullUrl);
-      return (await response.json()) as {
-        record: any;
-        attachments?: string[];
-        timestamp: number;
-      }[];
-    } catch (error) {
-      throw error;
-    }
-  } catch (error) {
-    console.log(`Key not found: ${deviceKey}.`);
-    console.log(error);
-    throw error;
-  }
+		try {
+			let response = await fetchUrl(fullUrl);
+			return await response.json() as { record: any, attachments?: string[], timestamp: number }[];
+		} catch (error) {
+			throw error;
+		}
+	} catch (error) {
+		console.log(`Key not found: ${deviceKey}.`);
+		console.log(error);
+		throw error;
+	}
 }
 
 export async function getAttachment(baseUrl: string, deviceKey: string, attachmentID: string) {
-  try {
-    if (!validateKey(deviceKey)) {
-      throw new Error('Bad key provided.');
-    }
+	try {
+		if (!validateKey(deviceKey)) {
+			throw new Error('Bad key provided.');
+		}
 
-    const response = await fetch(`${baseUrl}/attachment/${deviceKey}/${attachmentID}`, {
-      method: 'GET'
-    });
+		const response = await fetch(`${baseUrl}/attachment/${deviceKey}/${attachmentID}`, {
+			method: 'GET'
+		});
 
-    const blob = await response.blob();
+		const blob = await response.blob();
 
-    // Check for the attachment name
-    let fileName = response.headers.get('Attachment-Name');
-    // If the header is not present, fetch the attachment name
-    if (!fileName) {
-      // Fetch the attachment name
-      const nameResponse = await fetch(`${baseUrl}/attachment/${deviceKey}/${attachmentID}/name`, {
-        method: 'GET'
-      });
-      fileName = await nameResponse.text();
-    }
-    return { blob, fileName };
-  } catch (error) {
-    console.error('Error occurred during getAttachment request:', error);
-    throw error; // re-throw the error if you want to handle it further up the call stack
-  }
+		// Check for the attachment name
+		let fileName = response.headers.get('Attachment-Name');
+		// If the header is not present, fetch the attachment name
+		if (!fileName) {
+			// Fetch the attachment name
+			const nameResponse = await fetch(`${baseUrl}/attachment/${deviceKey}/${attachmentID}/name`, {
+				method: 'GET'
+			});
+			fileName = await nameResponse.text();
+		}
+		return { blob, fileName };
+	} catch (error) {
+		console.error('Error occurred during getAttachment request:', error);
+		throw error; // re-throw the error if you want to handle it further up the call stack
+	}
 }
 
 export async function postProvenance(deviceKey: string, record: any, attachments: readonly File[]) {
-  if (!validateKey(deviceKey)) {
-    throw new Error('Bad key provided.');
-  }
+	if (!validateKey(deviceKey)) {
+		throw new Error('Bad key provided.');
+	}
 
-  const baseUrl = useRuntimeConfig().public.baseUrl;
-  const formData = new FormData();
-  formData.append('provenanceRecord', JSON.stringify(record));
-  for (const blob of attachments) {
-    formData.append(blob.name, blob);
-  }
+	const baseUrl = useRuntimeConfig().public.baseUrl;
+	const formData = new FormData();
+	formData.append('provenanceRecord', JSON.stringify(record));
+	for (const blob of attachments) {
+		formData.append(blob.name, blob);
+	}
 
-  const fullUrl = baseUrl + '/provenance/' + deviceKey;
-  try {
-    let response = await fetchUrl(fullUrl, formData);
-    return (await response.json()) as { record: string; attachments?: string[] };
-  } catch (error) {
-    throw error;
-  }
+	const fullUrl = baseUrl + '/provenance/' + deviceKey;
+	try {
+		let response = await fetchUrl(fullUrl, formData);
+		return (await response.json()) as { record: string; attachments?: string[] };
+	} catch (error) {
+		throw error;
+	}
 }
 
 export async function postEmail(email: string) {
-  const baseUrl = useRuntimeConfig().public.baseUrl;
-  const formData = new FormData();
-  formData.append('email', email);
+	const baseUrl = useRuntimeConfig().public.baseUrl;
+	const formData = new FormData();
+	formData.append('email', email);
 
-  // backend urls are converted to all lowercase on deployment
-  const response = await fetch(`${baseUrl}/feedbackvolunteer`, {
-    method: 'POST',
-    body: formData
-  });
-  if (response.status != 200) {
-    throw new Error('postEmail: Failed to save email address');
-  }
+	// backend urls are converted to all lowercase on deployment
+	const response = await fetch(`${baseUrl}/feedbackvolunteer`, {
+		method: 'POST',
+		body: formData
+	});
+	if (response.status != 200) {
+		throw new Error('postEmail: Failed to save email address');
+	}
 }
 
 export async function getStatistics() {
-  const baseUrl = useRuntimeConfig().public.baseUrl;
-  const response = await fetch(`${baseUrl}/statistics`, {
-    method: 'GET'
-  });
-  return (await response.json()) as { record: string; timestamp: number }[];
+	const baseUrl = useRuntimeConfig().public.baseUrl;
+	const response = await fetch(`${baseUrl}/statistics`, {
+		method: 'GET'
+	});
+	return (await response.json()) as { record: string; timestamp: number }[];
 }
 
 async function fetchUrl(url: string, formData?: FormData) {
-  let response = undefined;
+	let response = undefined;
 
-  for (let i = 1; i <= 3; i++) {
-    try {
-      if (typeof formData !== 'undefined') {
-        response = await fetch(`${url}`, {
-          method: 'POST',
-          body: formData
-        });
-      } else {
-        response = await fetch(`${url}`, {
-          method: 'GET'
-        });
-      }
+	for (let i = 1; i <= 3; i++) {
+		try {
+			if (typeof formData !== 'undefined') {
+				response = await fetch(`${url}`, {
+					method: 'POST',
+					body: formData
+				});
+			} else {
+				response = await fetch(`${url}`, {
+					method: 'GET'
+				});
+			}
 
-      if (response !== undefined && response.status == 200) {
-        return response;
-      }
-    } catch (e) {
-      console.log('Fetch attempt failed: ' + e);
-    }
-  }
+			if (response !== undefined && response.status == 200) {
+				return response;
+			}
+		} catch (e) {
+			console.log('Fetch attempt failed: ' + e);
+		}
+	}
 
-  if (response !== undefined && response.status !== 200) {
-    console.log(`Failed to post provenance: ${response.status} ${response.statusText}`);
-    throw new Error(response.status + ' ' + response.statusText);
-  } else {
-    throw new Error(
-      `Could not connect to the server, check your internet connection and try again`
-    );
-  }
+	if (response !== undefined && response.status !== 200) {
+		console.log(`Failed to post provenance: ${response.status} ${response.statusText}`);
+		throw new Error(response.status + ' ' + response.statusText);
+	} else {
+		throw new Error(
+			`Could not connect to the server, check your internet connection and try again`
+		);
+	}
 }
 
 export async function cacheRequest(formUrl: string, formData: FormData) {
-  // Convert values to string and store them
-  let valuesToStore = [];
-  valuesToStore.push(['formUrl', formUrl]);
-  valuesToStore.push(['provenanceRecord', formData.get('provenanceRecord')]);
+	// Convert values to string and store them
+	let valuesToStore = [];
+	valuesToStore.push(['formUrl', formUrl]);
+	valuesToStore.push(['provenanceRecord', formData.get('provenanceRecord')]);
 
-  // Get cache_counter and add 1 to it
-  let current_request = localStorage.getItem('cache_counter');
-  if (current_request == null) {
-    current_request = '0';
-  }
-  let cache_counter = parseInt(current_request) + 1;
-  localStorage.setItem('cache_counter', cache_counter.toString());
+	// Get cache_counter and add 1 to it
+	let current_request = localStorage.getItem('cache_counter');
+	if (current_request == null) {
+		current_request = '0';
+	}
+	let cache_counter = parseInt(current_request) + 1;
+	localStorage.setItem('cache_counter', cache_counter.toString());
 
-  // Store the request at a unique key (gosqas_offline_cache_#)
-  let request_name = 'gosqas_offline_cache_' + cache_counter;
-  localStorage.setItem(request_name, JSON.stringify(valuesToStore));
+	// Store the request at a unique key (gosqas_offline_cache_#)
+	let request_name = 'gosqas_offline_cache_' + cache_counter;
+	localStorage.setItem(request_name, JSON.stringify(valuesToStore));
 }
--- packages/frontend/test/unitTests/azureFuncs.spec.ts
+++ packages/frontend/test/unitTests/azureFuncs.spec.ts
@@ -3,100 +3,100 @@ import { cacheRequest } from '~/services/azureFuncs';
 import * as z from 'zod';
 
 describe('Tests to see if requests can be cached', () => {
-  it('Test to see if returned data types are correct', async () => {
-    // Reset the naming counter (for testing purposes)
-    localStorage.setItem('cache_counter', '0');
+	it('Test to see if returned data types are correct', async () => {
+		// Reset the naming counter (for testing purposes)
+		localStorage.setItem('cache_counter', '0');
 
-    const baseUrl = useRuntimeConfig().public.baseUrl;
-    const deviceKey = await makeEncodedDeviceKey();
-    const formUrl = baseUrl + '/record/' + deviceKey;
-    const record = {
-      blobType: 'deviceInitializer',
-      deviceName: 'name',
-      description: 'description',
-      tags: [],
-      children_key: '',
-      hasParent: false,
-      isReportingKey: false
-    };
-    const formData = new FormData();
-    formData.append('provenanceRecord', JSON.stringify(record));
+		const baseUrl = useRuntimeConfig().public.baseUrl;
+		const deviceKey = await makeEncodedDeviceKey();
+		const formUrl = baseUrl + '/record/' + deviceKey;
+		const record = {
+			blobType: 'deviceInitializer',
+			deviceName: 'name',
+			description: 'description',
+			tags: [],
+			children_key: '',
+			hasParent: false,
+			isReportingKey: false
+		};
+		const formData = new FormData();
+		formData.append('provenanceRecord', JSON.stringify(record));
 
-    cacheRequest(formUrl, formData);
-    let requestFromCache = JSON.parse(localStorage.getItem('gosqas_offline_cache_1'));
+		cacheRequest(formUrl, formData);
+		let requestFromCache = JSON.parse(localStorage.getItem('gosqas_offline_cache_1'));
 
-    // Confirm that the datatypes are the same as they started
-    const returnedFormUrl = requestFromCache[0][1];
-    const returnedFormData = JSON.parse(requestFromCache[1][1]);
-    expect(typeof returnedFormUrl).toEqual(typeof formUrl);
-    expect(returnedFormUrl).toEqual(formUrl);
+		// Confirm that the datatypes are the same as they started
+		const returnedFormUrl = requestFromCache[0][1];
+		const returnedFormData = JSON.parse(requestFromCache[1][1]);
+		expect(typeof returnedFormUrl).toEqual(typeof formUrl);
+		expect(returnedFormUrl).toEqual(formUrl);
 
-    // Convert returned request back to FormData (stored in localStorage as string)
-    const formData2 = new FormData();
-    formData2.append('provenanceRecord', JSON.stringify(returnedFormData));
-    expect(formData2).toStrictEqual(formData);
+		// Convert returned request back to FormData (stored in localStorage as string)
+		const formData2 = new FormData();
+		formData2.append('provenanceRecord', JSON.stringify(returnedFormData));
+		expect(formData2).toStrictEqual(formData);
 
-    // Validate that the formData has the correct format
-    const ValidFormData = z.object({
-      blobType: z.string(),
-      deviceName: z.string().optional(),
-      description: z.string(),
-      tags: z.array(z.string()),
-      children_key: z.union([z.string(), z.array(z.string())]),
-      hasParent: z.boolean().optional(),
-      isReportingKey: z.boolean().optional()
-    });
-    ValidFormData.parse(returnedFormData);
-  });
+		// Validate that the formData has the correct format
+		const ValidFormData = z.object({
+			blobType: z.string(),
+			deviceName: z.string().optional(),
+			description: z.string(),
+			tags: z.array(z.string()),
+			children_key: z.union([z.string(), z.array(z.string())]),
+			hasParent: z.boolean().optional(),
+			isReportingKey: z.boolean().optional()
+		});
+		ValidFormData.parse(returnedFormData);
+	});
 
-  it('Test to see if we can store multiple requests', async () => {
-    localStorage.setItem('cache_counter', '0');
+	it('Test to see if we can store multiple requests', async () => {
+		localStorage.setItem('cache_counter', '0');
 
-    const baseUrl = useRuntimeConfig().public.baseUrl;
-    const deviceKey = await makeEncodedDeviceKey();
-    const formUrl = baseUrl + '/record/' + deviceKey;
-    const record = {
-      blobType: 'deviceInitializer',
-      deviceName: 'name',
-      description: 'description',
-      tags: [],
-      children_key: '',
-      hasParent: false,
-      isReportingKey: false
-    };
-    const record2 = {
-      blobType: 'deviceInitializer',
-      deviceName: 'name2',
-      description: 'slightly longer description',
-      tags: [],
-      children_key: '',
-      hasParent: false,
-      isReportingKey: false
-    };
+		const baseUrl = useRuntimeConfig().public.baseUrl;
+		const deviceKey = await makeEncodedDeviceKey();
+		const formUrl = baseUrl + '/record/' + deviceKey;
+		const record = {
+			blobType: 'deviceInitializer',
+			deviceName: 'name',
+			description: 'description',
+			tags: [],
+			children_key: '',
+			hasParent: false,
+			isReportingKey: false
+		};
+		const record2 = {
+			blobType: 'deviceInitializer',
+			deviceName: 'name2',
+			description: 'slightly longer description',
+			tags: [],
+			children_key: '',
+			hasParent: false,
+			isReportingKey: false
+		};
 
-    // Store two records in localStorage and confirm that they were stored correctly
-    const formData = new FormData();
-    const formData2 = new FormData();
-    formData.append('provenanceRecord', JSON.stringify(record));
-    formData2.append('provenanceRecord', JSON.stringify(record2));
+		// Store two records in localStorage and confirm that they were stored correctly
+		const formData = new FormData();
+		const formData2 = new FormData();
+		formData.append('provenanceRecord', JSON.stringify(record));
+		formData2.append('provenanceRecord', JSON.stringify(record2));
 
-    cacheRequest(formUrl, formData);
-    cacheRequest(formUrl, formData2);
+		cacheRequest(formUrl, formData);
+		cacheRequest(formUrl, formData2);
 
-    let requestFromCache = JSON.parse(localStorage.getItem('gosqas_offline_cache_1'));
-    const returnedFormUrl = requestFromCache[0][1];
-    const returnedFormData = JSON.parse(requestFromCache[1][1]);
-    expect(returnedFormUrl).toEqual(formUrl);
+		let requestFromCache = JSON.parse(localStorage.getItem('gosqas_offline_cache_1'));
+		const returnedFormUrl = requestFromCache[0][1];
+		const returnedFormData = JSON.parse(requestFromCache[1][1]);
+		expect(returnedFormUrl).toEqual(formUrl);
 
-    let requestFromCache2 = JSON.parse(localStorage.getItem('gosqas_offline_cache_2'));
-    const returnedFormUrl2 = requestFromCache2[0][1];
-    const returnedFormData2 = JSON.parse(requestFromCache2[1][1]);
-    expect(returnedFormUrl2).toEqual(formUrl);
+		let requestFromCache2 = JSON.parse(localStorage.getItem('gosqas_offline_cache_2'));
+		const returnedFormUrl2 = requestFromCache2[0][1];
+		const returnedFormData2 = JSON.parse(requestFromCache2[1][1]);
+		expect(returnedFormUrl2).toEqual(formUrl);
 
-    // Check that the correct record was stored at each request
-    expect(returnedFormData.deviceName).toEqual('name');
-    expect(returnedFormData2.deviceName).toEqual('name2');
-    expect(returnedFormData.description).toEqual('description');
-    expect(returnedFormData2.description).toEqual('slightly longer description');
-  });
+		// Check that the correct record was stored at each request
+		expect(returnedFormData.deviceName).toEqual('name');
+		expect(returnedFormData2.deviceName).toEqual('name2');
+		expect(returnedFormData.description).toEqual('description');
+		expect(returnedFormData2.description).toEqual('slightly longer description');
+	});
 });
