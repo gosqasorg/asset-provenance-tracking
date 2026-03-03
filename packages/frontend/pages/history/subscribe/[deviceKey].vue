41df77d19c24f0f3522955f8bfb895c38f15d58b
Mish Wilson <mishwilsonk@gmail.com>
2026-03-02 20:08:26 -0500

    moved to email-verify to subscribe/[deviceKey]

---
+++ packages/frontend/pages/history/subscribe/[deviceKey].vue
@@ -0,0 +1,29 @@
+<template>
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <div>
+
+        <!-- TODO: part 1 email input -->
+        <!-- submit btn -> sendCode() -->
+
+        <!-- TODO: part 2 - verify email -->
+        <!-- lock email field? -->
+        <!-- code input -->
+        <!-- verify btn  -->
+        <!-- resend code btn -->
+
+        <!-- TODO: part 3 success -->
+        <!-- confirmation msg -->
+
+        <!-- Maybe no fail? security issue -->
+    </div>
+</template>
+
+<script setup lang="ts">
+// TODO: verification states
+
+// TODO: sendCode()
+// POST to postNotificationEmail
+
+// TODO: verifyCode()
+// POST to VerifyCode
+</script>
