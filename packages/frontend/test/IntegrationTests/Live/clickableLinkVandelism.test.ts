26d63d5ba73bc482c6cbb22a1a3b2275e5e06223
Mish Wilson <mishwilsonk@gmail.com>
2026-03-17 15:08:07 -0400

    Commented plans

---
+++ packages/frontend/test/IntegrationTests/Live/clickableLinkVandelism.test.ts
@@ -0,0 +1,21 @@
+import { describe, it, expect } from "vitest";
+import clickableLink from "../../../utils/clickableLink";
+
+// Fucn to fetch device key
+
+// func to create record
+ 
+// func the fectch description
+
+describe('clickableLink: Vandalism Integration Tests', () => {
+
+
+    // TODO: Legitamate usages
+
+    // TODO: Script Injection <script> tag
+
+    // TODO: HTML Tag Injections - img, svg, style
+
+    // TODO: Alert Injection
+
+})
