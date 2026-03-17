f8aa5b91f01b2f4d3872cf9e155aa56e25edfaf0
Mish Wilson <mishwilsonk@gmail.com>
2026-03-17 15:33:31 -0400

    Util functions based on prev tests

--- packages/frontend/test/IntegrationTests/Live/clickableLinkVandelism.test.ts
+++ packages/frontend/test/IntegrationTests/Live/clickableLinkVandelism.test.ts
@@ -1,11 +1,50 @@
 import { describe, it, expect } from "vitest";
 import clickableLink from "../../../utils/clickableLink";
 
-// Fucn to fetch device key
+const baseUrl = 'https://gdtprodbackend.azurewebsites.net/api/provenance/';
 
-// func to create record
- 
-// func the fectch description
+// Grab unique device key from the backend.
+async function getDeviceKey(): Promise<string> {
+    const response = await fetch('https://gdtprodbackend.azurewebsites.net/api/getNewDeviceKey');
+    return response.text();
+}
+
+// func to create record 
+// Create a record with a given description, and return the device key used in record 
+async function createRecord(description: string): Promise<string> {
+    const deviceKey = await getDeviceKey();
+    let fullUrl = `${baseUrl}${deviceKey}`;
+
+    const formData = new FormData();
+    formData.append("provenanceRecord", JSON.stringify({
+        blobType: 'deviceInitializer',
+        deviceName: "Vandilism Test Record",
+        description,
+        tags: {},
+        children_key: '',
+        hasParent: false,
+        isReportingKey: false,
+    }));
+
+    const postResponse = await fetch(fullUrl, {
+        method: "POST",
+        body: formData,
+    });
+
+    expect(postResponse.ok).toBe(true);
+
+    return deviceKey;
+}
+
+async function fecthDescription (deviceKey: string): Promise<string> {
+    let fullUrl = `${baseUrl}${deviceKey}`;
+    let getResponse = await fetch(fullUrl); 
+    let data = await getResponse.json();
+
+    let responseString = JSON.parse(JSON.stringify(data[0]));
+    return responseString.record.description;
+}
+
 
 describe('clickableLink: Vandalism Integration Tests', () => {
 
