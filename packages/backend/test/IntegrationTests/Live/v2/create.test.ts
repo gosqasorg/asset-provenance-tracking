9fc00e35da0d35d0a7286b55fb20c35829a110ae
JaraR <jara_r1993@yahoo.com>
2026-04-01 15:41:25 -0400

    implemented main logic to batch retrieve, extract, and properly store child keys and child titles

--- packages/backend/test/IntegrationTests/Live/v2/create.test.ts
+++ packages/backend/test/IntegrationTests/Live/v2/create.test.ts
@@ -33,64 +33,73 @@ describe("Group Creation v2 tests", () => {
 
 	it("Custom Record Titles", async () => {
 		const baseUrl = "http://localhost:7071/api";
-        const customTitles = ["a1", "b2"];
+        const groupParentRecords = []
+        const groupedChildKeys = []
+        const groupedChildTitles = []
+
+        // Test cases are built as follows, note the children_name key can be left out or an empty array used as a value:
+        // [ deviceName,
+        // description,
+        // number_of_children,
+        // children_name ]
         const testCases = [
             [
-                "1st Test",
+                "0th Test",
                 "1 custom record title",
                 1,
                 ["a1"]
             ],
             [
-                "2nd Test",
+                "1st Test",
                 "3 custom record titles",
                 3,
                 ["a1", "b2", "c3"]
             ],
             [
-                "3rd Test",
-                "3 children, 2 custom record titles",
+                "2nd Test",
+                "3 children, 1 custom record title, 2 records without custom title",
                 3,
-                ["a1", "b2"]
+                ["a1"]
             ],
             [
-                "4th Test",
+                "3rd Test",
                 "3 children, empty children_name array",
                 3,
                 []
             ],
             [
-                "5th Test",
-                "3 children, no children_name array",
-                3
+                "4th Test",
+                "4 children, no children_name array",
+                4
             ],
             [
                 "",
-                "6th Test: completely empty parent deviceName string, no children_name array",
-                3
+                "5th Test: completely empty parent deviceName string, no children_name array",
+                5
             ],
             [
                 " ",
-                "7th Test: single space as parent deviceName string, no children_name array",
-                3
+                "6th Test: single space as parent deviceName string, no children_name array",
+                6
             ],
             [
-                "8th Test",
+                "7th Test",
                 "non-array children_name value, will fail Zod validation",
                 3,
                 "a"
             ],
             [
-                "9th Test",
+                "8th Test",
                 "children_name array with unexpected number, will fail Zod validation",
                 3,
                 ["a1", 9, "c3"]
             ]
         ]
-        const groupParentRecords = []
-        let testCase;
-
+        
         for (let i = 0; i < testCases.length; i++) {
+            let testCase;
+
+            // Creates group records based on whether or not test case contains children_name field"
             if (testCases[i].length === 4) {
                 testCase = await fetch(`${baseUrl}/createGroup`, {
                     method: "POST",
@@ -113,21 +122,30 @@ describe("Group Creation v2 tests", () => {
             }
 
             // Checks if fetch() response was sucessful (status in 200 - 299), then prepares to pull children
-            // test case 8 & 9 are meant to fail
+            // test case 7 & 8 are meant to fail
             if (i < 7){
                 expect(testCase.ok).toBe(true);
                 
                 let url = (await testCase.json()).groupUrl;
-                console.log(`Custom title test case #${i + 1}: ${url}`)
+                console.log(`Custom title test case #${i}: ${url}`)
 
-                let key = url.substring(url.lastIndexOf('/') + 1);
-                let prov = await (await fetch(`${baseUrl}/provenance/${key}`)).json();
-                groupParentRecords.push(prov[0].record)
+                let parentKey = url.substring(url.lastIndexOf('/') + 1);
+                let prov = await (await fetch(`${baseUrl}/provenance/${parentKey}`)).json();
+                let parentRecord = prov[0].record
+                groupParentRecords.push(parentRecord);
+
+                let childKeys = parentRecord.children_key
+                groupedChildKeys.push(childKeys);
+                
+                let tempGroup = []
+                for (let j = 0; j < childKeys.length; j ++) {
+                    let childProv = await (await fetch(`${baseUrl}/provenance/${childKeys[j]}`)).json();
+                    tempGroup.push(childProv[0].record.deviceName)
+                }
+                groupedChildTitles.push(tempGroup)
             } else { 
                 expect(testCase.ok).toBe(false);
             }
-
-            
         };
 
         // const response = await fetch(`${baseUrl}/createGroup`, {
@@ -158,8 +176,9 @@ describe("Group Creation v2 tests", () => {
         // console.log(prov[0].record);
         // console.log(prov[0].record.children_key[0])
         // console.log(await (await fetch(`${baseUrl}/provenance/${prov[0].record.children_key[0]}`)).json())
-        console.log(groupParentRecords)
-
+        console.log("groupParentRecords: ", groupParentRecords)
+        console.log("groupedChildKeys: ", groupedChildKeys)
+        console.log("groupedChildTitles: ", groupedChildTitles)
 	});
 
 	// More tests
