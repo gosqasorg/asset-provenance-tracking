c0a1d865dc2ecc3008542287ad1292679ed6ff21
JaraR <jara_r1993@yahoo.com>
2026-04-01 03:18:00 -0400

    batch test case record creation and fetch() response status testing

--- packages/backend/test/IntegrationTests/Live/v2/create.test.ts
+++ packages/backend/test/IntegrationTests/Live/v2/create.test.ts
@@ -55,50 +55,110 @@ describe("Group Creation v2 tests", () => {
             ],
             [
                 "4th Test",
-                "3 children, no custom record titles",
-                3
-            ],
-            [
-                "5th Test",
                 "3 children, empty children_name array",
                 3,
                 []
+            ],
+            [
+                "5th Test",
+                "3 children, no children_name array",
+                3
+            ],
+            [
+                "",
+                "6th Test: completely empty parent deviceName string, no children_name array",
+                3
+            ],
+            [
+                " ",
+                "7th Test: single space as parent deviceName string, no children_name array",
+                3
+            ],
+            [
+                "8th Test",
+                "non-array children_name value, will fail Zod validation",
+                3,
+                "a"
+            ],
+            [
+                "9th Test",
+                "children_name array with unexpected number, will fail Zod validation",
+                3,
+                ["a1", 9, "c3"]
             ]
-            
         ]
+        const groupParentRecords = []
+        let testCase;
 
-        // for (let i = 0; i < groupRecords.length; i++) {
+        for (let i = 0; i < testCases.length; i++) {
+            if (testCases[i].length === 4) {
+                testCase = await fetch(`${baseUrl}/createGroup`, {
+                    method: "POST",
+                    body: JSON.stringify({
+                        deviceName: testCases[i][0],
+                        description: testCases[i][1],
+                        number_of_children: testCases[i][2],
+                        children_name: testCases[i][3]
+                    })
+                });
+            } else {
+                testCase = await fetch(`${baseUrl}/createGroup`, {
+                    method: "POST",
+                    body: JSON.stringify({
+                        deviceName: testCases[i][0],
+                        description: testCases[i][1],
+                        number_of_children: testCases[i][2]
+                    })
+                });
+            }
 
-        // };
+            // Checks if fetch() response was sucessful (status in 200 - 299), then prepares to pull children
+            // test case 8 & 9 are meant to fail
+            if (i < 7){
+                expect(testCase.ok).toBe(true);
+                
+                let url = (await testCase.json()).groupUrl;
+                console.log(`Custom title test case #${i + 1}: ${url}`)
 
-        const response = await fetch(`${baseUrl}/createGroup`, {
-            method: "POST",
-            body: JSON.stringify({
-                deviceName: "Group Title",
-                description: "Group Description",
-                number_of_children: 3,
-                children_name: customTitles
-            })
-        });
+                let key = url.substring(url.lastIndexOf('/') + 1);
+                let prov = await (await fetch(`${baseUrl}/provenance/${key}`)).json();
+                groupParentRecords.push(prov[0].record)
+            } else { 
+                expect(testCase.ok).toBe(false);
+            }
 
-        let url = (await response.json()).groupUrl;
-        let key = url.substring(url.lastIndexOf('/') + 1);
-        let prov = await (await fetch(`${baseUrl}/provenance/${key}`)).json();
+            
+        };
 
-        // TODO: delete below
-        console.log("int test:", url);
-        console.log("resp:", response);
-        console.log(response.ok);
-        console.log(response.body);
-        console.log(response.status);
-        console.log(response.headers);
-        // console.log(await response.text())
-        console.log(url);
-        console.log(key);
+        // const response = await fetch(`${baseUrl}/createGroup`, {
+        //     method: "POST",
+        //     body: JSON.stringify({
+        //         deviceName: "Group Title",
+        //         description: "Group Description",
+        //         number_of_children: 3,
+        //         children_name: customTitles
+        //     })
+        // });
 
-        console.log(prov[0].record);
-        console.log(prov[0].record.children_key[0])
-        console.log(await (await fetch(`${baseUrl}/provenance/${prov[0].record.children_key[0]}`)).json())
+        // let url = (await response.json()).groupUrl;
+        // let key = url.substring(url.lastIndexOf('/') + 1);
+        // let prov = await (await fetch(`${baseUrl}/provenance/${key}`)).json();
+
+        // // TODO: delete below
+        // console.log("int test:", url);
+        // console.log("resp:", response);
+        // console.log(response.ok);
+        // console.log(response.body);
+        // console.log(response.status);
+        // console.log(response.headers);
+        // // console.log(await response.text())
+        // console.log(url);
+        // console.log(key);
+
+        // console.log(prov[0].record);
+        // console.log(prov[0].record.children_key[0])
+        // console.log(await (await fetch(`${baseUrl}/provenance/${prov[0].record.children_key[0]}`)).json())
+        console.log(groupParentRecords)
 
 	});
 
