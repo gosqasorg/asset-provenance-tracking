0ca8ba03d76c19d5eec5a33643ae4127fbdb91a6
Alison Gilpatrick <alisongilpatrick@icloud.com>
2026-04-14 17:38:49 -0400

    cleaned up comments added zod test

--- packages/backend/test/IntegrationTests/Live/v1/createRecord.test.ts
+++ packages/backend/test/IntegrationTests/Live/v1/createRecord.test.ts
@@ -16,12 +16,11 @@ describe("Backend Record Creation Tests", () => {
         const postValues = { "provenanceRecord": record, "attachment": [] }
 
         const recordResponse = await fetch(`${baseUrl}createRecord`, { method: "POST", body: JSON.stringify(postValues) });
-        console.log("RECORDRESP", recordResponse)
         expect(recordResponse.status).toBe(200)
 
         let recordUrl = (await recordResponse.json()).recordUrl;
         let deviceKey = recordUrl.substr(recordUrl.length - 22);
-        console.log("Created Basic Record (url): ", recordUrl.recordUrl)
+        console.log("Created Basic Record (url): ", recordUrl)
 
 		try {
 			let getResponse = await fetch(`${baseUrl}provenance/${deviceKey}`);
@@ -37,7 +36,7 @@ describe("Backend Record Creation Tests", () => {
 			expect(responseString.record.isReportingKey).toBe(false);
 
 		} catch(error) {
-			console.error('(Basic Create Test) Failed to fetch url: ' + recordUrl.recordUrl + '\nError: ' + error) 
+			console.error('(Basic Create Test) Failed to fetch url: ' + recordUrl + '\nError: ' + error) 
 			throw error;
 		}
     });
@@ -60,7 +59,7 @@ describe("Backend Record Creation Tests", () => {
 
         let recordUrl = (await recordResponse.json()).recordUrl;
         let deviceKey = recordUrl.substr(recordUrl.length - 22);
-        console.log("Created Record With Tags (url): ", recordUrl.recordUrl)
+        console.log("Created Record With Tags (url): ", recordUrl)
 
 		try {
 			let getResponse = await fetch(`${baseUrl}provenance/${deviceKey}`);
@@ -124,17 +123,69 @@ describe("Backend Record Creation Tests", () => {
             // Download attachment and confirm it matches original file
             const attachmentHash = responseString.attachments[0];
             const downloadUrl = `${baseUrl}attachment/${deviceKey}/${attachmentHash}`;
-            console.log('Downloading file from:', downloadUrl);
+            console.log('createRecord downloading file from:', downloadUrl);
             const downloadResponse = await fetch(downloadUrl);
             expect(downloadResponse.ok).toBe(true);
             const retrievedBuffer = Buffer.from(await downloadResponse.arrayBuffer());
             
             // Reading original large file 
             const originalBuffer = await readFile('./test/attachments/a200.jpg');
-            expect(Buffer.compare(originalBuffer, retrievedBuffer)).toBe(0);
+            expect(Buffer.compare(originalBuffer as Uint8Array<ArrayBufferLike>, retrievedBuffer as Uint8Array<ArrayBufferLike>)).toBe(0);
 
 		} catch(error) {
-			console.error('(Create With Attachment Test) Failed to fetch url: ' + recordUrl.recordUrl + '\nError: ' + error) 
+			console.error('(Create With Attachment Test) Failed to fetch url: ' + recordUrl + '\nError: ' + error) 
+			throw error;
+		}
+    });
+
+    it("Create A Record with Incorrect Format", async () => {
+        const baseUrl = 'https://gosqasbe.azurewebsites.net/api/';
+        // create a record that's missing a required field and confirm it fails (description)
+        const record = {
+            blobType: 'deviceInitializer',
+            deviceName: "Create Record Test",
+            tags: ['zod_test'],
+            children_key: '',
+            hasParent: false,
+            isReportingKey: false,
+        }
+
+        const postValues = { "provenanceRecord": record, "attachment": [] }
+        let recordResponse = await fetch(`${baseUrl}createRecord`, { method: "POST", body: JSON.stringify(postValues) });
+        expect(recordResponse.status).toBe(400);
+
+        // create a record that's missing an optional field and confirm it succeeds (blobType)
+        const record2 = {
+            deviceName: "Create Record Test",
+            description: "A test to make sure zod is correctly filtering input",
+            tags: ['zod_test'],
+            children_key: '',
+            hasParent: false,
+            isReportingKey: false,
+        }
+
+        const postValues2 = { "provenanceRecord": record2, "attachment": [] }
+        recordResponse = await fetch(`${baseUrl}createRecord`, { method: "POST", body: JSON.stringify(postValues2) });
+        expect(recordResponse.status).toBe(200);
+
+        let recordUrl = (await recordResponse.json()).recordUrl;
+        let deviceKey = recordUrl.substr(recordUrl.length - 22);
+        console.log("Created Record With Missing Optional Field (url): ", recordUrl)
+
+		try {
+			let getResponse = await fetch(`${baseUrl}provenance/${deviceKey}`);
+			getResponse = (await getResponse.json())[0];
+			let responseString = JSON.parse(JSON.stringify(getResponse));
+
+			expect(JSON.stringify(getResponse)).not.toBe('[]');
+			expect(responseString.record.deviceName).toBe('Create Record Test');
+			expect(responseString.record.description).toBe('A test to make sure zod is correctly filtering input');
+			expect(responseString.record.tags).toEqual(["zod_test"]);
+			expect(responseString.record.hasParent).toBe(false);
+			expect(responseString.record.isReportingKey).toBe(false);
+
+		} catch(error) {
+			console.error('(Create With Missing Optional) Failed to fetch url: ' + recordUrl + '\nError: ' + error) 
 			throw error;
 		}
     });
