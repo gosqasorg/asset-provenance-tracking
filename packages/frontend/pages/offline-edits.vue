9fd35706ff54dd87526c695ff4c9127874278830
hieungogh <soopersonic@yahoo.com>
2026-05-30 12:36:41 -0700

    Fixed requested changes and moved css code into its own offline-edits css file

--- packages/frontend/pages/offline-edits.vue
+++ packages/frontend/pages/offline-edits.vue
@@ -27,10 +27,38 @@ while offline.
         </div>
     </div>
 
-    <button class="btn dismiss all" style="width: 19%; margin-right: 15px" @click="clearAllEdits">Dismiss all edits</button>
-    <button class="btn dismiss published" style="width: 27%" @click="clearPublishedEdits">Dismiss published edits</button>
+    <button class="btn dismiss all" style="width: 19%; margin-right: 15px" @click="dismissAllEditsPopUp=true">Dismiss all edits</button>
+    <button class="btn dismiss published" style="width: 27%" @click="dismissPublishedEditsPopUp=true">Dismiss published edits</button>
 
-    <!----------------Queued Keys-------------------->
+    <!----------------Dismiss All Edits Popup-------------------->
+    <div class="popup" v-if="dismissAllEditsPopUp">
+        <div class="popup-inner">
+            <h3 style="font-weight: bold">You're dismissing all of your offline edits</h3>
+            <p class="inner-p">Once you dismiss your offline edits, they will no longer be available for review.</p>
+            <button class="popup-buttons" style="float: left;" @click="dismissAllEditsPopUp=false">Go back</button>
+            <button class="btn confirm-dismiss" style="float: right" @click="clearAllEdits(); dismissAllEditsPopUp=false">Dismiss all edits</button>
+        </div>
+    </div>
+
+    <!----------------Dismiss Published Edits Popup-------------->
+    <div class="popup" v-if="dismissPublishedEditsPopUp">
+        <div class="popup-inner">
+            <h3 style="font-weight: bold">You're dismissing your published offline edits</h3>
+            <p class="inner-p">Once you dismiss your published offline edits, they will no longer be available for review.</p>
+            <button class="popup-buttons" style="float: left;" @click="dismissPublishedEditsPopUp=false">Go back</button>
+            <button class="btn confirm-dismiss" style="float: right" @click="clearPublishedEdits()">Dismiss published edits</button>
+        </div>
+    </div>
+    <!----------------Dimiss an Offline Edit Popup--------------->
+    <div class="popup" v-if="dismissSingleEditPopUp">
+        <div class="popup-inner">
+            <h3 style="font-weight: bold">You're dismissing an offline edit</h3>
+            <p class="inner-p">You're dismissing the below edit. Once dismissed, it will no longer be available for review.</p>
+            <button class="popup-buttons" style="float: left;" @click="dismissSingleEditPopUp=false">Go back</button>
+            <button class="btn confirm-dismiss" style="float: right" @click="clearOneEdit()">Dismiss edit</button>
+        </div>
+    </div>
+    <!----------------Queued Keys Banner-------------------->
     <div v-for="(key, index) in offlineKeys">
         <div class="key-box">
             <p style="font-size: 17px;">{{ key }}</p>
@@ -38,17 +66,17 @@ while offline.
         </div>
     </div>
 
-    <!----------------Published Keys------------------>
+    <!----------------Published Keys Banner------------------>
     <div v-for="(key, index) in fulfilledKeys">
         <div class="key-box" style="border: none; overflow:auto; display:grid; gap:10px; margin-bottom: 10px; margin-top: 0px">
             <p style="grid-row: 1; font-size: 17px; margin-top: -5px; margin-bottom: -5px">{{ key }}</p>      
             <div class="status-bubble">Published</div>
-            <button class="btn key-buttons" style="grid-row: 1;">Go to record</button>
-            <button class="btn key-buttons bottom" @click="clearEdit(key)">Dismiss edit</button>
+            <button class="btn key-buttons" style="grid-row: 1;" @click="$router.push('/history/' + key)">Go to record</button>
+            <button class="btn key-buttons bottom" @click="dismissSingleEditPopUp=true; clearOneEditPrepare(key)">Dismiss edit</button>
         </div>
     </div>
 
-    <!----------------Syncing Keys-------------------->
+    <!----------------Syncing Keys Banner-------------------->
     <div v-for="(key, index) in getSyncingKeys">
         <div class="key-box sync" style="border: solid; border-color: #efcc9b;">
             <p style="font-size: 17px">{{ key }}</p>
@@ -56,7 +84,7 @@ while offline.
         </div>
     </div>
 
-    <!------------------Failed Keys------------------->
+    <!------------------Failed Keys Banner------------------->
     <div v-for="(key, index) in getFailedKeys">
         <div class="key-box failed" style="border: solid; overflow: auto; display: grid; gap: 10px; margin-bottom: 10px; margin-top: 0px; border-color: #ebb9b6;">
             <p style="grid-row: 1; font-size: 17px;">{{ key }}</p>
@@ -70,9 +98,17 @@ while offline.
 </template>
 
 <script lang="ts">
+import { RouterView, useRouter } from 'vue-router';
+
+const router = useRouter();
+
 export default {
 data() {
 	return {
+        dismissOneKey: '',
+        dismissAllEditsPopUp: false,
+        dismissPublishedEditsPopUp: false,
+        dismissSingleEditPopUp: false,
         offlineKeys: [] as string[],
         fulfilledKeys: [] as string[],
         syncingKeys: [] as string[],
@@ -86,6 +122,7 @@ async mounted() {
         this.getOfflineKeys();
         this.getSyncingKeys();
         this.getFulfilledKeys();
+        this.clearOneEdit();
     } catch (e) {
         console.log("There was an error displaying your offline edits: " + e)
     }
@@ -93,7 +130,7 @@ async mounted() {
 
 methods: {
     getOfflineKeys() {
-        // Get all keys that were successfully created while offline
+        // Get all keys that were successfully stashed while offline
         let stash_counter = parseInt(localStorage.getItem('stash_counter') || "0");
         for (stash_counter; stash_counter > 0; stash_counter--) {
             let test = JSON.parse(localStorage.getItem('gosqas_offline_stash_' + stash_counter) || '{}')
@@ -119,178 +156,33 @@ methods: {
 
     },
     clearAllEdits() {
-        if (confirm("Are you sure you want to dismiss all edits?")) {
-            localStorage.setItem("gdt-stash-fulfilled", "")
-            window.location.reload();           
-        }
+        localStorage.setItem('stash_counter', '0');
+        localStorage.setItem('gdt-stash-fulfilled', '')
+        window.location.reload();
     },
     clearPublishedEdits() {
-        if (confirm("Are you sure you want to dismiss all published edit?")) {
-            localStorage.setItem("gdt-stash-fulfilled", "")
-            window.location.reload();  
-        }
+        localStorage.setItem("gdt-stash-fulfilled", "")
+        window.location.reload();  
     },
-    clearEdit(key: string) {
-        if (confirm("Are you sure you want to clear this edit?")) {
-            const index = this.fulfilledKeys.indexOf(key);
-            if (index > -1) {
-                this.fulfilledKeys.splice(index, 1);
-            }
+    clearOneEditPrepare(key: string) {
+        this.dismissOneKey = key;
+    },
+    clearOneEdit() {
+        // Removes key from fulfilled array then resets and copies this array to gdt stash fullfilled
+        const index = this.fulfilledKeys.indexOf(this.dismissOneKey);
+        if (index > -1) {
+            this.fulfilledKeys.splice(index, 1);
+            this.dismissOneKey = '';
+            this.dismissSingleEditPopUp = false;
         }
+        localStorage.setItem('gdt-stash-fulfilled', '')
+        localStorage.setItem('gdt-stash-fulfilled', this.fulfilledKeys.toString())
+    },
     }
-}
 }
 </script>
 
-<style>
-.row {
-    margin-top:32px;
-}
-
-/* For screens smaller than 768px */
-#donate-container{
-    padding: 50px 20px 40px 20px;
-}
-
-.dismiss {
-    margin-bottom: 20px;
-    border-radius: 11px;
-    padding: 12px 16px;
-}
-.status-bubble {
-    grid-row:2; 
-    background-color: #00dc82; 
-    border-radius: 20px; 
-    width: 115px; 
-    color: black; 
-    text-align: center; 
-    display:flex; 
-    justify-content: center; 
-    align-items: center;
-    margin-top: -2px;
-    height: 40px;
-}
-.key-buttons {
-    width: 100px;
-    width: 50%;
-    border-radius: 6px;
-    font-size: 14px;
-    padding-top: 10px;
-    margin-bottom: 5px;
-    justify-self: end;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 10px;
-}
-
-/* Wrap buttons once screen gets below a certain size */
-@media (max-width: 1024px) {
-.dismiss {
-	width: 100% !important;
-}
-.key-box {
-    display:flex !important;
-    flex-direction: column !important;
-}
-.status-bubble {
-    padding: 10px;
-}
-.key-buttons {
-    width: 100% !important;
-    margin-top: auto !important;
-    
-}
-}
-
-/* Dark mode version*/
-@media (prefers-color-scheme: dark) {
-    h1 {
-        color: #CCECFD;
-    }
-    p {
-        color: #FFFFFF;
-    }
-    .row {
-        margin-bottom: 30px;
-        color: white;
-    }
-    .btn {
-        background-color: #CCECFD;
-        border: #CCECFD;
-        color: black;
-    }
-    .btn:hover,
-    .btn:active {
-        background-color: #e6f6ff;
-        color: black;
-    }
-    .key-buttons.bottom:hover,
-    .key-buttons.bottom:active {
-        background-color: #636262 !important;
-    }
-    .key-box {
-        color: white;
-        padding: 20px;
-        margin-bottom: 14px;
-        margin-top: 14px;
-        border: solid;
-        border-color: #91bdf5;
-        border-radius: 20px;
-        border-width: 2px;
-        word-wrap: break-word;
-        background-color: #383838;
-    }
-    .btn.key-buttons.bottom {
-        grid-row: 2; 
-        border: solid; 
-        border-width: 1px; 
-        border-color: #ffffff; 
-        background-color: #383838; 
-        color: #ffffff
-    }
-}
-/* Light mode version*/
-@media (prefers-color-scheme: light) {
-    h1 {
-        color: #4E3681;
-    }
-    p {
-        color: #1E2019;
-    }
-    .row {
-        margin-bottom: 30px;
-        color: black;
-    }
-    .btn {
-        background-color: #4e3681;
-        border: #4e3681;
-        color: white;
-    }
-    .btn:hover,
-    .btn:active {
-        background-color: #322253 !important;
-        color: white !important;
-    }
-    .key-box {
-        color: white;
-        padding: 20px;
-        margin-bottom: 14px;
-        margin-top: 14px;
-        border: solid;
-        border-color: #bbd7f9;
-        border-radius: 20px;
-        border-width: 2px;
-        word-wrap: break-word;
-        background-color: #f0f3f8;
-    }
-    .key-buttons.bottom {
-        grid-row: 2; 
-        border: solid; 
-        border-width: 2px; 
-        border-color: #322253; 
-        background-color: #f0f3f8; 
-        color: #322253
-    }
-}
+<style scoped>
+    @import '../assets/css/history-form.css';
+    @import '../assets/css/offline-edits.css';
 </style>
