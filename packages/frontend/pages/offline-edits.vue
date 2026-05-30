4229ab7083d7b413bdaca04b3e13b91e691a16b8
hieungogh <soopersonic@yahoo.com>
2026-05-30 15:22:49 -0700

    Changed border-width of sync and failed banners

--- packages/frontend/pages/offline-edits.vue
+++ packages/frontend/pages/offline-edits.vue
@@ -67,7 +67,7 @@ while offline.
     </div>
 
     <!----------------Published Keys Banner------------------>
-    <div v-for="(key, index) in fulfilledKeys">
+    <div v-for="(key, index) in offlineKeys">
         <div class="key-box" style="border: none; overflow:auto; display:grid; gap:10px; margin-bottom: 10px; margin-top: 0px">
             <p style="grid-row: 1; font-size: 17px; margin-top: -5px; margin-bottom: -5px">{{ key }}</p>      
             <div class="status-bubble">Published</div>
@@ -77,16 +77,16 @@ while offline.
     </div>
 
     <!----------------Syncing Keys Banner-------------------->
-    <div v-for="(key, index) in getSyncingKeys">
-        <div class="key-box sync" style="border: solid; border-color: #efcc9b;">
+    <div v-for="(key, index) in offlineKeys">
+        <div class="key-box sync" style="border: solid; border-width: 2px; border-color: #efcc9b;">
             <p style="font-size: 17px">{{ key }}</p>
             <div class="status-bubble" style="background-color: #df892a;">Syncing</div>
         </div>
     </div>
 
     <!------------------Failed Keys Banner------------------->
-    <div v-for="(key, index) in getFailedKeys">
-        <div class="key-box failed" style="border: solid; overflow: auto; display: grid; gap: 10px; margin-bottom: 10px; margin-top: 0px; border-color: #ebb9b6;">
+    <div v-for="(key, index) in offlineKeys">
+        <div class="key-box failed" style="border: solid; border-width: 2px; overflow: auto; display: grid; gap: 10px; margin-bottom: 10px; margin-top: 0px; border-color: #ebb9b6;">
             <p style="grid-row: 1; font-size: 17px;">{{ key }}</p>
             <div class="status-bubble" style="background-color: #e08a82;">Failed</div>
             <button class="btn key-buttons" style="grid-row: 1;">Retry syncing</button>
@@ -109,10 +109,10 @@ data() {
         dismissAllEditsPopUp: false,
         dismissPublishedEditsPopUp: false,
         dismissSingleEditPopUp: false,
-        offlineKeys: [] as string[],
-        fulfilledKeys: [] as string[],
-        syncingKeys: [] as string[],
-        failedKeys: [] as string[],
+        offlineKeys: ['laskdfjaslkdfsd'] as string[],
+        fulfilledKeys: ['laskdfjaslkdfsd'] as string[],
+        syncingKeys: ['laskdfjaslkdfsd'] as string[],
+        failedKeys: ['laskdfjaslkdfsd'] as string[],
 	}
 },
 
