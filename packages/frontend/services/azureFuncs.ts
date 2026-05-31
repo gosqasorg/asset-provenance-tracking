bb564032d5426c75fed2180c2538505242e8d7e2
hieungogh <soopersonic@yahoo.com>
2026-05-31 09:37:04 -0700

    Removed lazytoggle line

--- packages/frontend/services/azureFuncs.ts
+++ packages/frontend/services/azureFuncs.ts
@@ -13,7 +13,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import { LazyButtonsLargeToggle } from "#components";
 import { validateKey } from "~/utils/keyFuncs";
 
 // Feature flag to turn ON/OFF Offline Mode features while in development
