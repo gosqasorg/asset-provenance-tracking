d191722249ca2555f46699b4a2d6b39e2c6ced22
Vincent Steffens <VinceSteffens@gmail.com>
2026-06-24 15:31:15 -0700

    Set email to gosqas azure email

--- packages/backend/src/functions/httpTrigger.ts
+++ packages/backend/src/functions/httpTrigger.ts
@@ -935,7 +935,8 @@ export async function postNotificationEmail(request: HttpRequest, context: Invoc
             const { sendEmail } = await import('./sendEmail.js'); //  This prevents the top-level code in sendEmail.ts from running at startup.
             
             const emailResult = await sendEmail(
-                "DoNotReply@091bd21c-5093-45ed-9479-ad92fef9d66e.azurecomm.net",
+                //"DoNotReply@091bd21c-5093-45ed-9479-ad92fef9d66e.azurecomm.net",
+                "DoNotReply@8577d69b-9011-4385-abec-cfe9325dbfe6.azurecomm.net",
                 email,
                 "GOSQAS Verification Code",
                 `Your verification code is: ${code} \n\nOr click this link to verify automatically:${verifyLink} \nExpires in 10 minutes.\nIf you didn't request this, ignore this email.`,
