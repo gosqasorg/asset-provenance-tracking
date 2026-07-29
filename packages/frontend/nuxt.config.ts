6fb06c2c718ae07c6954006910dc803d0ce49135
Vincent Steffens <VinceSteffens@gmail.com>
2026-07-28 19:06:28 -0700

    Whitelist --> regex to capture all files

--- packages/frontend/nuxt.config.ts
+++ packages/frontend/nuxt.config.ts
@@ -87,7 +87,7 @@ frontendUrl: ${nuxt.options.runtimeConfig.public.frontendUrl}`);
       clientsClaim: true,
       globPatterns: ['**/*.{js,css,html,png,svg,ico,vue,mjs}'],
       maximumFileSizeToCacheInBytes: 5 * 1024 ** 2,
-      navigateFallbackDenylist: [/^\/GDT-OpenAPI-Spec\.yaml$/, /^\/user_manual\.pdf$/, /^\/Global_Distributed_Tracking\.pdf$/]
+      navigateFallbackDenylist: [/\/[^/?]+\.[^/]+$/]
     },
     devOptions: {
       enabled: false
