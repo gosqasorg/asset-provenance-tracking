9e7d6c12fe9091154e69a6584ece2e91f2e49a4d
Vincent Steffens <VinceSteffens@gmail.com>
2026-08-24 15:50:32 -0700

    Get microtest working again

--- packages/backend/src/functions/httpTriggerUtils.ts
+++ packages/backend/src/functions/httpTriggerUtils.ts
@@ -4,14 +4,10 @@ import sharp from 'sharp'
 
 // Setup credentials
 let KEY1 = process.env.KEY1
-let KEY2 = process.env.KEY2
-let LOCATION = process.env.LOCATION
 let ENDPOINT = process.env.ENDPOINT
 
 // Check credentials
 if(! [KEY1, 
-      KEY2,
-      LOCATION, 
       ENDPOINT
      ].every(Boolean)
 ) { console.error('Error: credentials not set'); process.exit(1) }
@@ -77,7 +73,7 @@ export async function downscaleIfApplicableAndBase64Encode(inputFileBuffer: Buff
 }
 
 // returns true if image is ok, return false if image is flagged
-export async function checkImageAgainstContentModerationAPI(base64File: string/*, KEY1, ENDPOINT*/) {
+export async function checkImageAgainstContentModerationAPI(base64File: string) {
     if(! (typeof base64File == 'string') ){ throw new Error('not got: string') }
 
     // Build json
--- packages/backend/test/Microtest/microtest.spec.ts
+++ packages/backend/test/Microtest/microtest.spec.ts
@@ -6,17 +6,46 @@ Code is not to be kept here. This is a whiteboard. Erase and add your own
 as needed. 
 */
 
+
 // Do not remove. Vitest Requirement
 import { describe, it, expect, vi, beforeEach } from 'vitest';
 // Do not remove. Vitest Requirement
 
+/* ================================================================= * 
+   README: If you are reading this, you may delete what's here.
+   That being said, I used what was here previously as an example.
+   It was useful. What's left here was done in the hopes it may be.
+
+   This working "development-driven testing" environment is, I find,
+   massively useful for dev.
+
+   To run it, ./cycle-microtest.sh to initiate the test loop. Once 
+   the loop is active, vitest tests this directory every 5 seconds.
+
+   To use it, detach the terminal and leave it running where it's at
+   least partially in view.
+
+   To stop it, simply hit ctrl + c. 
+
+   Then, code away. 
+   If you hit an error, you know immediately. 
+   Once it's solved, you know immediately. 
+
+   Code tends to originate here, and migrate elsewhere in two types:
+   * Utilities / functions / non-test code, and
+   * You get at least one test out of it for free. That test is
+     set up and ready to be slotted into place. 
+ * ================================================================= */
+
+
 /* ------ Actual Imports -------- */
 
+// These came right along into the test file
 import { readFile } from 'node:fs/promises';
 import * as htUtils from '../../src/functions/httpTriggerUtils';
 
 
-/* ================= Utility Functions ======================== */
+/* ================= Utility Functions Supporting The Test ======== */
 
 async function readTheFile(inputFileName: String) {
   const buffer = await readFile(inputFileName)
@@ -25,12 +54,10 @@ async function readTheFile(inputFileName: String) {
   return theFileObject
 }
 
-async function setup() {
-  let inputFileName = 'test/Microtest/oversize-g-rated.jpeg'
-  let fileObject = await readTheFile(inputFileName)
-  return fileObject
-}
 
+/* ================== Function Under Development ================= */
+
+// Relocated to httpTriggerUtils.ts
 export async function convertFileForSharp(inputFileObject: File) {
   if (! (inputFileObject instanceof File) ){ throw new Error('Not got: File') }
 
@@ -41,17 +68,13 @@ export async function convertFileForSharp(inputFileObject: File) {
   return theBuffer
 }
 
+
 /* ========================= The Test ============================ */
 
+// Note: no need to delete the enclosing describe block
 describe('MicroTestLand', () => {
-  it('AzureContentSafetyAPITest', async () => {
 
-    let fileObject = await setup()
-    let response = await htUtils.imageIsNotPermitted(fileObject, console)
-    expect(response).toBe(false)
-
-  });
-
+  // Relocated to tests/UnitTests/httpTriggerUtils.spec.ts
   it('Determine whether file is image', async () => {
     let fileObject, classification;
 
@@ -65,4 +88,6 @@ describe('MicroTestLand', () => {
     classification = await htUtils.isImage(fileObject, console)
     expect(classification).toBe(false)
   })
+
+// closing brace of describe block
 });
