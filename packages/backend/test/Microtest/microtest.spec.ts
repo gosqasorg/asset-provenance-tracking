c8676dd144a7652d692c151b66d866413249af32
Vincent Steffens <VinceSteffens@gmail.com>
2026-08-24 13:45:43 -0700

    Tests ready to be migrated to test dir

--- packages/backend/test/Microtest/microtest.spec.ts
+++ packages/backend/test/Microtest/microtest.spec.ts
@@ -18,10 +18,10 @@ import * as htUtils from '../../src/functions/httpTriggerUtils';
 
 /* ================= Utility Functions ======================== */
 
-async function readTheFile(inputFileName: String): File {
+async function readTheFile(inputFileName: String) {
   const buffer = await readFile(inputFileName)
   let theFileObject = await new File([buffer], inputFileName)
-  if(! (theFileObject instanceof File) ){ throw new Error('not made: File') }
+  if(! (theFileObject instanceof File) ){ throw new Error('not made File') }
   return theFileObject
 }
 
@@ -31,6 +31,15 @@ async function setup() {
   return fileObject
 }
 
+export async function convertFileForSharp(inputFileObject: File) {
+  if (! (inputFileObject instanceof File) ){ throw new Error('Not got: File') }
+
+  console.log(inputFileObject.constructor.name, Object.keys(inputFileObject))
+  console.log(inputFileObject.constructor.name, Object.keys(inputFileObject), inputFileObject.size)
+  let theBuffer = await Buffer.from(await inputFileObject.arrayBuffer())
+  if(! (theBuffer instanceof Buffer) ){ throw new Error('not made: buffer') }
+  return theBuffer
+}
 
 /* ========================= The Test ============================ */
 
@@ -38,8 +47,22 @@ describe('MicroTestLand', () => {
   it('AzureContentSafetyAPITest', async () => {
 
     let fileObject = await setup()
-    let response = await htUtils.contentModerationImageCheck(fileObject)
-    expect(response).toBe('Pass')
+    let response = await htUtils.imageIsNotPermitted(fileObject, console)
+    expect(response).toBe(false)
 
   });
+
+  it('Determine whether file is image', async () => {
+    let fileObject, classification;
+
+    // Real image
+    fileObject = await readTheFile('test/Microtest/oversize-g-rated.jpeg')
+    classification = await htUtils.isImage(fileObject, console)
+    expect(classification).toBe(true)
+
+    // PDF
+    fileObject = await readTheFile('test/Microtest/Powerset-lattice-3.pdf')
+    classification = await htUtils.isImage(fileObject, console)
+    expect(classification).toBe(false)
+  })
 });
